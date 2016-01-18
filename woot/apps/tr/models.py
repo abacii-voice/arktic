a72f239e988e293d517c1a822d72e5ceac4efcc0
NicholasPiano <nicholas.d.piano@gmail.com>
2016-01-18 18:20:49 +0000

    adding classes

--- woot/apps/tr/models.py
+++ woot/apps/tr/models.py
@@ -9,4 +9,24 @@ from django.db import models
 
 
 ### Create your models here.
-class
+class Recogniser(models.Model):
+class Client(models.Model):
+class Project(models.Model):
+class Batch(models.Model):
+class Grammar(models.Model):
+class GrammarInstance(models.Model):
+class GrammarFile(models.Model):
+class Transcription(models.Model):
+class AudioFile(models.Model):
+class RecognisedUtterance(models.Model):
+class Utterance(models.Model):
+class UserComment(models.Model):
+class IssueTag(models.Model):
+class Word(models.Model):
+class Tag(models.Model):
+class ClientTag(models.Model):
+	### Connections
+
+	### Properties
+
+	pass
