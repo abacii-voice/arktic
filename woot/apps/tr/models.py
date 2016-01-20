c1afb6fb7e252c2f104b9a66bd238d9909a942f4
NicholasPiano <nicholas.d.piano@gmail.com>
2016-01-20 14:08:04 +0000

    gathering and branching for all app models.

--- woot/apps/tr/models.py
+++ woot/apps/tr/models.py
@@ -10,20 +10,35 @@ from django.db import models
 
 ### Create your models here.
 class Recogniser(models.Model):
+	pass
 class Client(models.Model):
+	pass
 class Project(models.Model):
+	pass
 class Batch(models.Model):
+	pass
 class Grammar(models.Model):
+	pass
 class GrammarInstance(models.Model):
+	pass
 class GrammarFile(models.Model):
+	pass
 class Transcription(models.Model):
+	pass
 class AudioFile(models.Model):
+	pass
 class RecognisedUtterance(models.Model):
+	pass
 class Utterance(models.Model):
+	pass
 class UserComment(models.Model):
+	pass
 class IssueTag(models.Model):
+	pass
 class Word(models.Model):
+	pass
 class Tag(models.Model):
+	pass
 class ClientTag(models.Model):
 	### Connections
 
