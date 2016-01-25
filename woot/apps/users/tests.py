659eeb7c169e608c2098b76ea28aaa3954a5fc7d
NicholasPiano <nicholas.d.piano@gmail.com>
2016-01-25 14:01:01 +0000

    building basic test cases

--- woot/apps/users/tests.py
+++ woot/apps/users/tests.py
@@ -2,5 +2,16 @@
 from django.test import TestCase
 
 # local
+from apps.client.models.client import Client
+from apps.users.models.admin import Admin
+from apps.users.models.moderator import Moderator
+from apps.users.models.user import User
 
 # Create your tests here.
+class BasicUserTestCase(TestCase):
+	def setup(self):
+		client = Client.objects.create(name='test_client', is_production=True)
+		admin = Admin.objects.create(client=client, email='admin@test.com', first_name='admin_first', last_name='admin_last')
+
+	def test_basic_user_relationships(self):
+		pass
