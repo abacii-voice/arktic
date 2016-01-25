73b174b6f6e2d93b13ae280c6a58031a45414da4
NicholasPiano <nicholas.d.piano@gmail.com>
2016-01-25 13:40:35 +0000

    remove unnecessary import

--- woot/apps/users/models/base.py
+++ woot/apps/users/models/base.py
@@ -2,9 +2,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# local
-from apps.client.models.client import Client
-
 # util
 import uuid
 
