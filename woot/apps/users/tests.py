459674bfcdd2e3a03fb07318fb354c9cb24564e4
NicholasPiano <nicholas.d.piano@gmail.com>
2016-01-27 14:23:34 +0000

    writing tests

--- woot/apps/users/tests.py
+++ woot/apps/users/tests.py
@@ -14,37 +14,6 @@ class BasicUserTestCase(TestCase):
 		# client
 		client = Client.objects.create(name='test_client', is_production=True)
 
-		# superadmin
-		superadmin = Superadmin.objects.create(email='superadmin@test.com', first_name='superadmin_first', last_name='superadmin_last')
-		superadmin.set_password('superadmin_password')
-
-		# superadmin surrogate user
-		# superadmin_surrogate_user = 
-
-		# admin
-		admin = Admin.objects.create(client=client, email='admin@test.com', first_name='admin_first', last_name='admin_last')
-		admin.set_password('admin_password')
-
-		# moderator
-		moderator = Moderator.objects.create(client=client, email='moderator@test.com', first_name='moderator_first', last_name='moderator_last')
-		moderator.set_password('moderator_password')
-
-		# user
-		user = User.objects.create(client=client, user_moderator=moderator, email='user@test.com', first_name='user_first', last_name='user_last')
-		user.set_password('user_password')
-
 	def test_basic_user_relationships(self):
 		# get
-		client = Client.objects.get(name='test_client')
-		superadmin = Superadmin.objects.get(email='superadmin@test.com')
-		admin = Admin.objects.get(email='admin@test.com')
-		moderator = Moderator.objects.get(email='moderator@test.com')
-		user = User.objects.get(email='user@test.com')
-
-		# test OneToOneField relationship
-		self.assertEqual(superadmin.email, superadmin.base.email) # can access parent class object via OneToOneField
-		self.assertEqual(admin.email, admin.base.email)
-		self.assertEqual(moderator.email, moderator.base.email)
-		self.assertEqual(user.email, user.base.email)
-
-		# test surrogates
+		pass
