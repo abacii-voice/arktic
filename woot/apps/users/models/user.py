b059ed8228ce4051a4686c73d4f6f7630b9f0a3c
NicholasPiano <nicholas.d.piano@gmail.com>
2016-01-31 17:03:31 +0000

    stopping for sausage

--- woot/apps/users/models/user.py
+++ woot/apps/users/models/user.py
@@ -14,6 +14,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 	email = models.EmailField(max_length=255, unique=True)
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
+	activation_key = models.CharField(max_length=20) # use utils to generate unique key
 
 	# type
 	is_approved = models.BooleanField(default=False)
--- woot/urls.py
+++ woot/urls.py
@@ -3,16 +3,102 @@ from django.conf.urls import include, url
 from django.views.generic import TemplateView, RedirectView
 
 # local
-from apps.users.views import LoginView, AccountSPAView
+from apps.users.views import AdminSignupView, AccountSPAView, LoginView, logout_view
 
 urlpatterns = [
 	# i18n / l10n
 	url(r'^i18n/', include('django.conf.urls.i18n')),
+]
 
-	### Users
+### Users
+'''
+1. Entry point: Admin signup
+
+This is the first thing linked from the main page. When someone wants to "sign up", they will be directed to this view. It will allow them to choose an email, a password, and other contact details such as telephone number and the name and address of the company so that a contract can be set up between Abacii, Ltd. and the prospective client.
+
+An email will be sent to their email address to verify it. A randomly generated key will be used for the link.
+'''
+urlpatterns += [
+	url(r'^signup/', AdminSignupView.as_view()),
+]
+
+'''
+2. The account SPA
+
+Every user on the site will view the details of their account from this View. It will appear different for each user, but many different users will share similar information and interfaces.
+
+a. The Superadmin
+
+	Someone's got to have to control. In this case, the managers of the site (me) need a way to see who is currently signed up to use the site. It gives a summary of the clients using the site and their recent activity, including:
+
+	i. Signup activity
+	ii. Requests for account activation
+	iii. Addition of admins, moderators, and workers
+	iv. Projects active and completed
+	v. Hours, transcriptions, overwatch, and other metrics
+	vi. Hits to the site and traffic information
+
+b. The Admin
+
+	Here the admin can see the activity of their organisation. If the client is a contract client, it can only see progress. If it is a production client, it can also see progress and performance of each worker and moderator.
+
+c. Moderator
+
+	One level down from the admin. They can see their activity and billing numbers, but not those of the organisation as a whole. They can also see reports on each worker that reports to them.
+
+d. Worker
+
+	They can only see their personal billing numbers.
+
+'''
+urlpatterns += [
+	# account
+	url(r'^account/$', AccountSPAView.as_view()),
+]
+
+'''
+3. Fairly standard login page
+
+With one exception: a key can be given as an argument that can be used to verify the user. When a admin signs up, they will be sent an email containing a url with a key that will lead here.
+
+Workers and moderators will also get keys in their emails, but they do not sign up. Their accounts are created by the admin of a production client.
+'''
+
+urlpatterns += [
 	# login
 	url(r'^login/', LoginView.as_view()),
-
-	# account
-	url(r'^account/', AccountSPAView.as_view()),
+	url(r'^login/(?P<key>[A-Z0-9]{10})/$', LoginView.as_view()),
+	url(r'^logout/', logout_view),
 ]
+
+### Transcription
+'''
+1. The Transcription SPA
+
+The main transcription interface includes the following components:
+
+a. Transcription + (training, demo)
+b. Overwatch + (training, demo)
+c. Moderation + (training, demo)
+
+### main.js
+### |__ plugin.js
+
+transcription.js
+|__ transcription_training.js
+|__ transcription_demo.js
+
+overwatch.js
+|__ overwatch_training.js
+|__ overwatch_demo.js
+
+moderation.js
+|__ moderation_training.js
+|__ moderation_demo.js
+
+'''
+
+### Audio upload
+
+
+### Actions
