f1dd4fd94bb7d2ee5cc955602ae6aa80fd3f8e12
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-01 21:14:35 +0000

    Working on interfaces

--- woot/apps/users/forms.py
+++ woot/apps/users/forms.py
@@ -5,7 +5,12 @@ from django import forms
 
 ### User forms
 class NewAdminForm(forms.Form):
-	pass
+	email = forms.EmailField(label='Email', max_length=100)
+	first_name = forms.CharField(label='First name', max_length=100)
+	last_name = forms.CharField(label='Last name', max_length=100)
+	company_name = forms.CharField(label='Company name', max_length=100)
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+	password_repeat = forms.CharField(label='Password (again)', widget=forms.PasswordInput)
 
 class LoginForm(forms.Form):
 	pass
