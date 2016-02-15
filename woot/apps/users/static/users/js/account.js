94062e513fad9f9b3fb403a2be07f83375f770fb
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-15 21:02:20 +0000

    starting to come together

--- woot/apps/users/static/users/js/account.js
+++ woot/apps/users/static/users/js/account.js
@@ -1,13 +1,51 @@
+// Define global states
+UI.createGlobalState('client-state');
+UI.createGlobalState('role-state');
+UI.createGlobalState('content-state');
+
 // Contains a definition of the components in the Account app
-UI.createComponent('client-sidebar', {
-	classes:['sidebar'],
-	style:{},
-	statePrototypes: {
-		'client-state':{},
-		'role-state':{},
-		'content-state':{},
-	},
-	switchPrototypes: {
-		
+UI.createComponent('back-sidebar', {
+	args: {
+		template: UI.templates.sidebar,
+		classes: ['sidebar'],
+		style: {},
+		states: [
+			{name: 'client-state', args: {}},
+			{name: 'role-state', args: {}},
+			{name: 'content-state', args: {}},
+		],
+		switches: {
+
+		},
+	}
+	components: {
+		UI.createComponent('bs-back-button', {
+			args: {
+				template: UI.templates.button,
+				classes: ['button'],
+				style: {},
+				states: [
+
+				],
+				switches: {
+
+				},
+			},
+			components: {},
+		}),
+		UI.createComponent('bs-test-button', {
+			args: {
+				template: UI.templates.button,
+				classes: ['button'],
+				style: {},
+				states: [
+
+				],
+				switches: {
+
+				},
+			},
+			components: {},
+		}),
 	},
 });
