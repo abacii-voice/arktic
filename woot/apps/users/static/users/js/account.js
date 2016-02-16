0a27188c43312f8050485c5a52d077f1f8108e18
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-16 17:42:31 +0000

    css animation seems a little slow

--- woot/apps/users/static/users/js/account.js
+++ woot/apps/users/static/users/js/account.js
@@ -5,69 +5,151 @@ UI.createGlobalStates('client-state', [
 ]);
 
 // Contains a definition of the components in the Account app
-UI.createComponent('back-sidebar', {
+UI.createComponent('app', {
 	root: 'hook',
 	args: {
-		template: UI.templates.sidebar,
-		classes: 'mini',
-		style: {},
-		html: '',
-		states: [
-			{name: 'client-state', args: {
-				style: {
-					width: '200px',
-				},
-			}},
-			{name: 'role-state', args: {
-				style: {
-					width: '250px',
-				},
-			}},
-			{name: 'content-state', args: {}},
-		],
-		svitch: {
-
-		},
+		template: UI.templates.div,
 	},
 	children: [
-		UI.createComponent('bs-back-button', {
+		UI.createComponent('back-sidebar', {
 			args: {
-				template: UI.templates.button,
-				classes: 'button',
-				style: {},
-				html: 'Back button',
-				click: function (model) {
-					
+				template: UI.templates.sidebar,
+				classes: ['mini'],
+				style: {
+					'left': '-100px',
 				},
 				states: [
-					{name: 'client-state', args: {}},
-					{name: 'content-state', args: {}},
+					{name: 'client-state', args: {
+						style: {
+							'left': '-100px',
+						},
+					}},
+					{name: 'role-state', args: {
+						style: {
+							'left': '0px',
+						},
+					}},
 				],
-				svitch: {
-					'client-state':'role-state',
-					'role-state':'client-state',
-				},
 			},
-			children: [],
-		}),
-		UI.createComponent('bs-test-button', {
-			args: {
-				template: UI.templates.button,
-				classes: 'button',
-				style: {},
-				html: 'Test button',
-				states: [
-					{name: 'client-state', args: {}},
-					{name: 'content-state', args: {}},
-				],
-				svitch: {
+			children: [
+				UI.createComponent('bs-back-button', {
+					args: {
+						template: UI.templates.button,
+						classes: ['button'],
+						style: {},
+						html: 'Back button',
+						click: function (model) {
 
-				},
+						},
+						states: [
+							{name: 'client-state', args: {}},
+							{name: 'content-state', args: {}},
+						],
+						svitch: {
+							'client-state':'role-state',
+							'role-state':'client-state',
+						},
+					},
+					children: [],
+				}),
+				UI.createComponent('bs-test-button', {
+					args: {
+						template: UI.templates.button,
+						classes: ['button'],
+						style: {},
+						html: 'Test button',
+						states: [
+							{name: 'client-state', args: {}},
+							{name: 'content-state', args: {}},
+						],
+						svitch: {
+
+						},
+					},
+					children: [],
+				}),
+			],
+		}),
+		UI.createComponent('client-sidebar', {
+			args: {
+				template: UI.templates.sidebar,
+				classes: [''],
+				style: {},
+				states: [
+					{name: 'client-state', args: {
+						style: {
+							'left': '0px',
+						},
+					}},
+					{name: 'role-state', args: {
+						style: {
+							'left': '-300px',
+						},
+					}},
+				]
 			},
-			children: [],
+			children: [
+				UI.createComponent('cs-test-client-button', {
+					args: {
+						template: UI.templates.button,
+						classes: ['button'],
+						style: {},
+						html: 'Test button',
+						click: function (model) {
+
+						},
+						states: [],
+						svitch: {
+							'client-state':'role-state',
+							'role-state':'client-state',
+						},
+					},
+					children: [],
+				}),
+			],
+		}),
+		UI.createComponent('role-sidebar', {
+			args: {
+				template: UI.templates.sidebar,
+				classes: [''],
+				style: {
+					'left':'-300px',
+				},
+				states: [
+					{name: 'client-state', args: {
+						style: {
+							'left': '-300px',
+						},
+					}},
+					{name: 'role-state', args: {
+						style: {
+							'left': '50px',
+						},
+					}},
+				]
+			},
+			children: [
+				UI.createComponent('cs-test-role-button', {
+					args: {
+						template: UI.templates.button,
+						classes: ['button'],
+						style: {},
+						html: 'Test button',
+						click: function (model) {
+
+						},
+						states: [],
+						svitch: {
+							'client-state':'role-state',
+							'role-state':'client-state',
+						},
+					},
+					children: [],
+				}),
+			],
 		}),
 	],
-});
+})
 
 // Render to start
-UI.getComponent('back-sidebar').render();
+UI.getComponent('app').render();
