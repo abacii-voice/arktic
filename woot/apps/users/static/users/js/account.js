e8ad6205292417636ffb3e689b4142dc29b6d5c4
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-16 10:33:31 +0000

    now to test states.

--- woot/apps/users/static/users/js/account.js
+++ woot/apps/users/static/users/js/account.js
@@ -1,52 +1,64 @@
 // Define global states
-UI.createGlobalState('client-state');
-UI.createGlobalState('role-state');
-UI.createGlobalState('content-state');
+UI.createGlobalStates([
+	'client-state',
+	'role-state',
+	'content-state'
+]);
 
 // Contains a definition of the components in the Account app
 UI.createComponent('back-sidebar', {
 	root: 'hook',
 	args: {
 		template: UI.templates.sidebar,
-		classes: ['sidebar'],
+		classes: ['mini'],
 		style: {},
+		html: '',
 		states: [
 			{name: 'client-state', args: {}},
 			{name: 'role-state', args: {}},
 			{name: 'content-state', args: {}},
 		],
-		switches: [
+		svitches: [
 
 		],
 	},
-	components: [
+	children: [
 		UI.createComponent('bs-back-button', {
 			args: {
 				template: UI.templates.button,
 				classes: ['button'],
 				style: {},
+				html: 'Back button',
 				states: [
-
+					{name: 'client-state', args: {}},
+					{name: 'role-state', args: {}},
+					{name: 'content-state', args: {}},
 				],
-				switches: [
+				svitches: [
 
 				],
 			},
-			components: [],
+			children: [],
 		}),
 		UI.createComponent('bs-test-button', {
 			args: {
 				template: UI.templates.button,
 				classes: ['button'],
 				style: {},
+				html: 'Test button',
 				states: [
-
+					{name: 'client-state', args: {}},
+					{name: 'role-state', args: {}},
+					{name: 'content-state', args: {}},
 				],
-				switches: [
+				svitches: [
 
 				],
 			},
-			components: [],
+			children: [],
 		}),
 	],
 });
+
+// Render to start
+UI.getComponent('back-sidebar').render();
