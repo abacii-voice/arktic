66f0e6b81a5365c467f825c430e880f62efb947b
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-18 23:48:41 +0000

    replacing ui code

--- woot/assets/js/ui/ui.js
+++ woot/assets/js/ui/ui.js
@@ -1,205 +1,276 @@
+// UI: This is the UI definition. It should be 100% ignorant of the actual data passing through the app
+// (other than the structure of the components). It is not responsible for the context of the
+// interface, only the definitions of what is where, when.
+
 var UI = {
+	////////////
+	// GLOBAL STATE
+	// stores current global state
+	globalState: undefined,
 
-	///////// GLOBAL STATES
-	// these states define the state structure of the app.
-	// Components cannot have states that are not found in the global state list
-
-	// global state list
-	gState: '',
+	// stores list of possible global states. These constrain any states submitted by components.
 	globalStates: [],
 
-	// really simple method
-	createGlobalState: function (name) {
-		if (this.globalStates.indexOf(name) == -1) {
-			this.globalStates.push(name);
+	// shortcut to create a global state
+	createGlobalState: function (stateName) {
+		if (this.globalStates.indexOf(stateName) === -1) {
+			this.globalStates.push(stateName);
+			this.states[stateName] = []; // initialise arrays
+			this.svitches[stateName] = [];
 		}
 	},
 
-	createGlobalStates: function (initialState, states) {
-		// set initial state
-		this.gState = initialState;
-		this.createGlobalState(initialState);
-
-		states.map(function (name) {
-			this.createGlobalState(name);
-		}, this);
+	// create multiple global states with an array
+	createGlobalStates: function (homeState, stateNames) {
+		this.globalState = homeState;
+		stateNames.shift(homeState);
+		stateNames.map(createGlobalState, this);
 	},
 
-	///////// STATE CHANGES
-	changeState: function (name) {
-		UI.gState = name;
-		this.states.filter(function (state) { // filter the list of states by "name"
-			return state.name === name;
-		}).map(function (state) { // map to components
-			state.component.changeState(state);
-		});
+	// A change of global state that is broadcast to the components that need to respond.
+	changeState: function (stateName) {
+		if (this.globalStates.indexOf(stateName) !== -1) {
+			// update global state
+			this.globalState = stateName;
+
+			// execute all svitches with a promise
+			var svitchPromise = new Promise(function () {
+				UI.svitches[stateName].map(function (svitch) {
+					svitch.fn(svitch.component);
+				});
+			});
+
+			// when svitches are complete, change the state of every component with the state name
+			var stateFunction = function () {
+				UI.states[stateName].map(function (state) {
+					state.component.changeState(state);
+				});
+			}
+
+			// execute
+			$.when(svitchPromise).done(stateFunction);
+		}
 	},
 
-	///////// COMPONENTS
-	// This is the main component function
-	component: function (id, properties) {
-		// assign id that identifies both in the DOM and in UI.
+	// A chain of broadcasts made with a delay between them.
+	// If one delay is given, it will be used for all calls. If more than N-1 delays are given,
+	// only the first N-1 are used. Where N is the number of states.
+	chainStates: function (stateNames, delays) {
+
+	}
+
+	////////////
+	// COMPONENT
+	// List of components stored by id
+	components: {},
+	getComponent: function (componentId) {
+		return this.components[componentId];
+	}
+
+	// Basic component definition
+	component: function (id, args) {
+		// the variable 'this' refers to an instance of this function when called with 'new component()'
+
+		// id
 		this.id = id;
 
-		// root for rendering
-		this.root = properties.root;
+		// args
+		// The structure of this is not too important, but it should stay like this:
+		// (id, { // <- args variable
+		// 	root: '',
+		// 	properties: {
+		// 		props: {},
+		// 		template: UI.templates.*,
+		// 		html: '',
+		// 		classes: [],
+		// 		style: {},
+		// 		states: [],
+		// 		svtiches: [],
+		// 		stateMap: {},
+		// 		click: function () {},
+		// 	},
+		// 	children: [],
+		// })
 
-		// template
-		this.template = properties.args.template;
-		this.classes = properties.args.classes; // html classes
-		this.style = properties.args.style;
-		this.html = properties.args.html;
-		this.click = properties.args.click;
-		this.fn = properties.args.fn;
+		// root
+		this.root = args.root;
+
+		// static properties
+		this.props = args.properties.props;
+
+		// rendered with template
+		this.template = args.properties.template;
+		this.html = args.properties.html;
+		this.classes = args.properties.classes;
+		this.style = args.properties.style;
 
 		// states
-		if (properties.args.states !== undefined) {
-			this.states = properties.args.states.map(function (statePrototype) {
-				return UI.createState(statePrototype.name, this, statePrototype.args);
+		if (args.properties.states !== undefined) {
+			this.states = args.properties.states.map(function (state) {
+				return UI.createState(this, state.name, state.args);
 			}, this);
-			this.state = this.states[0]; // always start with the first state in the array
 		}
 
-		// svitches
-		this.svitch = properties.args.svitch;
+		if (args.properties.svtiches !== undefined) {
+			this.svitches = args.properties.svitches.map(function (svitch) {
+				return UI.createSvitch(this, svitch.stateName, svitch.fn);
+			});
+		}
 
-		// components
-		this.children = properties.children;
+		this.stateMap = args.properties.stateMap;
 
-		// render
+		// children
+		this.children = args.children;
+
+		// bindings
+		this.click = args.click;
+
+		///////////////
+		// PRE-PROCESSING
+		// model
 		this.model = function () {
 			return $('#{id}'.format({id: this.id}));
-		};
+		}
 
+		// render
 		this.render = function () {
+			// 1. root
 			var root = $('#{id}'.format({id: this.root}));
 
-			// 0. template
-			var template = this.template.format({
+			// 2. render template
+			var renderedTemplate = this.template.format({
 				id: this.id,
-				classes: UI.formatClasses(this.classes),
-				style: UI.formatStyle(this.style),
+				classes: formatClasses(this.classes),
+				style: formatStyle(this.style),
 				html: this.html,
-			})
+			});
 
-			// 1. add element to the DOM
-			if (root.children().length > 0) {
-				root.children().last().after(template); // place as last child
+			// 3. Add element to the DOM under root.
+			if (root.children().length !== 0) {
+				root.children().last().after(renderedTemplate); // add as last child
 			} else {
-				root.html(template); // simply place inside
+				root.html(renderedTemplate);
 			}
 
-			// 2. Bindings
+			// 4. add bindings
 			if (this.click !== undefined) {
-				var component = this;
-				var model = this.model();
+				var _this = this;
 
-				model.on('click', function (e) {
-					// change state if necessary
-					$.when(new Promise (function () {
-						// perform click function
-						component.click(model);
-					})).done(function () {
-						if (component.svitch[UI.gState] !== undefined) {
-							UI.changeState(component.svitch[UI.gState]);
-						}
-					});
+				_this.model().on('click', function () {
+					_this.click(_this);
 				});
 			}
 
-			var _this = this;
-			if (_this.fn !== undefined) {
-				$.when(_this.fn(_this)).done(function () {
-					if (_this.children !== undefined) {
-						_this.children.map(_this.renderChild, _this);
-					}
-				});
-			} else {
-				if (_this.children !== undefined) {
-					_this.children.map(_this.renderChild, _this);
-				}
+			// 5. render children
+			if (this.children !== undefined) {
+				this.children.map(this.renderChild, this);
 			}
-		};
+		}
 
+		// render child
 		this.renderChild = function (child) {
 			child.root = this.id;
 			child.render();
-		};
+		}
 
-		this.changeState = function (state) {
-			// set state
-			this.state = state;
+		///////////////
+		// STATE CHANGES
+		// change state
+		this.changeState = function (stateName) {
+			// 1. set new state
+			this.state = this.getState(stateName);
 
-			// apply changes to model
+			// 2. get model
 			var model = this.model();
 
-			// classes
-			var classes = this.state.hasOwnProperty('classes') ? this.state.classes : this.classes;
-			model.addClass(classes);
+			// 3. add classes
+			var classes = this.state.classes !== undefined ? this.state.classes : this.classes;
+			model.addClass(formatClasses(classes));
 
-			// style
-			var style = this.state.hasOwnProperty('style') ? this.state.style : this.style;
-			if (style !== undefined) {
-				model.animate(style);
+			// 4. add style
+			var style = this.state.style !== undefined ? this.state.style : this.style;
+			model.animate(style);
+
+			// 5. add html
+			if (this.state.html !== undefined) {
+				model.html(this.state.html);
 			}
 
-			// html
-			var html = this.state.hasOwnProperty('html') ? this.state.html : this.html;
-			if (html !== undefined) {
-				model.html(html);
-			}
-
-			// fn
+			// 6. perform action
 			if (this.state.fn !== undefined) {
 				this.state.fn(this);
 			}
-		};
+		}
+
+		this.mapState = function (stateName) {
+			return this.stateMap[stateName] !== undefined ? this.stateMap[stateName] : '';
+		}
+
+		///////////////
+		// UTILITY
+		// get state
+		this.getState = function (stateName) {
+			return this.states.filter(function (state) {
+				return state.name === stateName;
+			})[0];
+		}
+
 	},
 
-	// stores a list of components and their attributes
-	components:{},
-
-	// factory method that creates and returns a component, adding to the list of components
+	// component factory
 	createComponent: function (id, args) {
 		var component = new this.component(id, args);
-		this.components[id] = component;
+		components[id] = component;
 		return component;
-	},
+	}
 
-	getComponent: function (id) {
-		return this.components[id];
-	},
+	////////////
+	// COMPONENT STATE
+	// States grouped by name
+	states: {},
 
-	///////// STATES
-	// basic state object
-	state: function (name, component, args) {
-		// identify the state by its name and the component it belongs to.
-		this.name = name;
+	// Basic state definition
+	state: function (component, name, args) {
 		this.component = component;
+		this.name = name;
 
 		// args
 		this.classes = args.classes;
 		this.style = args.style;
 		this.html = args.html;
 		this.fn = args.fn;
+	}
+
+	// state factory
+	createState: function (component, name, args) {
+		var state = new this.state(component, name, args);
+		this.states[name].push(state);
+		return state;
+	}
+
+	////////////
+	// COMPONENT SVITCH
+	// Svitches are specific actions that must be carried out before a particular state change, such as a server API call.
+	// A svitch can be used to access its component or state or be filtered by state name
+	svitches: {},
+
+	// Svitch definition
+	svitch: function (component, stateName, fn) {
+		this.component = component;
+		this.stateName = stateName;
+		this.fn = fn;
 	},
 
-	// list of states
-	states:[],
-
-	// factory method that adds the state prototype to the array of states
-	createState: function (name, component, args) {
-		if (this.globalStates.indexOf(name) != -1) {
-			var state = new this.state(name, component, args);
-			this.states.push(state);
-			return state;
-		} else {
-			throw 'State name, {name}, must be in the global list of states: {states}'.format({name:name, states:this.globalStates});
-		}
+	// svitch factory
+	createSvitch: function (component, state, fn) {
+		var svitch = new this.svitch(component, stateName, fn);
+		this.svitches[stateName].push(svitch);
+		return svitch;
 	},
 
-	///////// TEMPLATES
-	// These are html templates that can be called
+	////////////
+	// COMPONENT TEMPLATES
+	//
 	templates: {
 		sidebar: `
 			<div id='{id}' class='sidebar {classes}' style='{style}'></div>
@@ -216,27 +287,37 @@ var UI = {
 		`,
 		div: `
 			<div id={id}></div>
-		`
-	},
+		`,
+	}
 
-	///////// FORMATTING
-	formatStyle: function (style) {
-		if (style !== undefined) {
-			var strings = Object.keys(style).map(function (value) {
-				return '{key}: {value}; '.format({key: value, value: style[value]})
-			});
-			return strings.join('');
-		} else {
-			return '';
-		}
-	},
+}
 
-	formatClasses: function (classes) {
-		if (classes !== undefined) {
-			return classes.join(' ');
-		} else {
-			return '';
+// CONTEXT: This is the context definition. It is a local storage of the relevant variables from
+// the server used to display the interface. It should make direct calls to the server API to gather data.
+
+var Context = {
+	////////////
+	// STORE
+	// The variable extracted from the server API call. All parsing of its meaning is done by the specific
+	// UI script for each page.
+	store: {},
+	get: function (args) {
+		// based on a string of parameters, access a value or other sub-structure from store.
+		sub = this.store;
+		var i;
+		for (i=0; i<args.length, i++) {
+			sub = sub[args[i]]
 		}
 	}
 
+	// define update function for context along with triggers and anything else.
+	fn: undefined,
+	setFn: function (fn) {
+		this.fn = fn;
+	},
+
+	// A custom function can be defined to update the store and even trigger a state based on the result.
+	update: function (args) {
+		this.fn(args); // args is not intended to be new data. This functionality should be encoded in this.fn.
+	},
 }
