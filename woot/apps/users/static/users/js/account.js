7c5f78a41101d62c08caf44a0d8c82bd801cc30d
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-19 14:44:34 +0000

    testing new class changes

--- woot/apps/users/static/users/js/account.js
+++ woot/apps/users/static/users/js/account.js
@@ -32,11 +32,47 @@ UI.createApp('hook', [
 	UI.createComponent('client-sidebar', {
 		properties: {
 			template: UI.templates.sidebar,
+
+			// default appearance
+			// style: {hidden},
+
+			// responds to client-state, otherwise hidden
 			states: [],
 			svtiches: [],
 		},
 	}),
-])
+	UI.createComponent('role-sidebar', {
+		properties: {
+			template: UI.templates.sidebar,
+			
+		},
+	}),
+	UI.createComponent('back-sidebar', {
+		properties: {
+			template: UI.templates.sidebar,
+			classes: ['mini'],
+
+			// default appearance
+			// style: {hidden},
+
+			// responds to client-state, otherwise hidden
+			states: [],
+		},
+		children: [
+			UI.createComponent('bs-back-button', {
+				properties: {
+					template: UI.templates.button,
+					html: '<span class="glyphicon glyphicon-chevron-left"></span>',
+					classes: ['button'],
+				}
+			}),
+		],
+	}),
+]);
+
+// Need more complex behaviour for classes:
+// 1. Adding a class from a state should remove the classes of other states
+// 2. Maybe have a stateClass variable that can be changed.
 
 // 4. Render app
 UI.renderApp();
