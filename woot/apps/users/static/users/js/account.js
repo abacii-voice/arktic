4acfc509252e0735f1a44f2521a8386f2a4e8a15
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-19 17:47:04 +0000

    minor corrections

--- woot/apps/users/static/users/js/account.js
+++ woot/apps/users/static/users/js/account.js
@@ -21,7 +21,7 @@ UI.createGlobalStates('client-state', [
 // 		classes: [],
 // 		style: {},
 // 		states: [],
-// 		svtiches: [],
+// 		svitches: [],
 // 		stateMap: {},
 // 		click: function () {},
 // 	},
@@ -55,7 +55,7 @@ UI.createApp('hook', [
 			UI.createComponent('cs-test-button', {
 				properties: {
 					template: UI.templates.button,
-					html: 'Test button',
+					html: 'Test client',
 					stateMap: {
 						'client-state': 'role-state',
 					},
@@ -87,6 +87,20 @@ UI.createApp('hook', [
 				}},
 			],
 		},
+		children: [
+			UI.createComponent('rs-test-button', {
+				properties: {
+					template: UI.templates.button,
+					html: 'Test role',
+					stateMap: {
+						'role-state': 'content-state',
+					},
+				},
+				click: function (_this) {
+					UI.changeState(_this.stateMap[UI.globalState]);
+				}
+			}),
+		],
 	}),
 	UI.createComponent('back-sidebar', {
 		properties: {
@@ -102,11 +116,17 @@ UI.createApp('hook', [
 					style: {
 						'left': '0px',
 					},
+					fn: function (_this) {
+						_this.model().animate({'left': '0px'}, 200);
+					}
 				}},
 				{name: 'content-state', args: {
 					style: {
-						'left': '0px',
+						'left': '-50px',
 					},
+					fn: function (_this) {
+						_this.model().animate({'left': '0px'}, 200);
+					}
 				}},
 			],
 		},
@@ -134,179 +154,3 @@ UI.createApp('hook', [
 
 // 4. Render app
 UI.renderApp();
-
-// // Contains a definition of the components in the Account app
-// UI.createComponent('app', {
-// 	root: 'hook',
-// 	args: {
-// 		template: UI.templates.div,
-// 	},
-// 	children: [
-// 		UI.createComponent('client-sidebar', {
-// 			args: {
-// 				template: UI.templates.sidebar,
-// 				classes: [''],
-// 				style: {},
-// 				fn: function (component) {
-// 					// make ajax call to get client data
-// 					return ajax('user_roles', {}, function (data) {
-// 						var clients = data['clients'];
-// 						Object.keys(clients).map(function (client) {
-// 							component.children.push(UI.createComponent('cs-{name}-button'.format({name: client}), {
-// 								args: {
-// 									template: UI.templates.button,
-// 									classes: ['button'],
-// 									style: {},
-// 									html: 'Client: {name}'.format({name: client}),
-// 									click: function (model) {
-// 										UI.getComponent('role-sidebar').model().attr('client', client);
-// 									},
-// 									states: [],
-// 									svitch: {
-// 										'client-state':'role-state',
-// 									},
-// 								}
-// 							}));
-// 						});
-// 					});
-// 				},
-// 				states: [
-// 					{name: 'client-state', args: {
-// 						style: {
-// 							'left': '0px',
-// 						},
-// 					}},
-// 					{name: 'role-state', args: {
-// 						style: {
-// 							'left': '-300px',
-// 						},
-// 					}},
-// 				]
-// 			},
-// 			children: [
-// 			],
-// 		}),
-// 		UI.createComponent('role-sidebar', {
-// 			args: {
-// 				template: UI.templates.sidebar,
-// 				classes: [''],
-// 				style: {
-// 					'left':'-300px',
-// 				},
-// 				states: [
-// 					{name: 'client-state', args: {
-// 						style: {
-// 							'left': '-300px',
-// 						},
-// 					}},
-// 					{name: 'role-state', args: {
-// 						style: {
-// 							'left': '50px',
-// 						},
-// 						fn: function (component) {
-// 							var client = component.model().attr('client');
-//
-// 						}
-// 					}},
-// 					{name: 'content-state', args: {
-// 						style: {
-// 							'left': '-300px',
-// 						},
-// 					}},
-// 				]
-// 			},
-// 			children: [
-// 				UI.createComponent('cs-test-role-button', {
-// 					args: {
-// 						template: UI.templates.button,
-// 						classes: ['button'],
-// 						style: {},
-// 						html: 'Test role',
-// 						click: function (model) {
-//
-// 						},
-// 						states: [],
-// 						svitch: {
-// 							'role-state':'content-state',
-// 						},
-// 					},
-// 					children: [],
-// 				}),
-// 			],
-// 		}),
-// 		UI.createComponent('back-sidebar', {
-// 			args: {
-// 				template: UI.templates.sidebar,
-// 				classes: ['mini'],
-// 				style: {
-// 					'left': '-100px',
-// 				},
-// 				states: [
-// 					{name: 'client-state', args: {
-// 						style: {
-// 							'left': '-100px',
-// 						},
-// 					}},
-// 					{name: 'role-state', args: {
-// 						style: {
-// 							'left': '0px',
-// 						},
-// 					}},
-// 				],
-// 			},
-// 			children: [
-// 				UI.createComponent('bs-back-button', {
-// 					args: {
-// 						template: UI.templates.button,
-// 						classes: ['button'],
-// 						style: {},
-// 						html: '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
-// 						click: function (model) {
-//
-// 						},
-// 						states: [
-// 							{name: 'client-state', args: {}},
-// 							{name: 'content-state', args: {}},
-// 						],
-// 						svitch: {
-// 							'role-state':'client-state',
-// 							'content-state':'role-state',
-// 						},
-// 					},
-// 					children: [],
-// 				}),
-// 			],
-// 		}),
-// 		UI.createComponent('content-panel', {
-// 			args: {
-// 				template: UI.templates.contentPanel,
-// 				classes: [],
-// 				style: {
-// 					'opacity': '0.0',
-// 				},
-// 				html:'<h1>Hello</h1>',
-// 				states: [
-// 					{name: 'client-state', args: {
-// 						style: {
-// 							'opacity': '0.0',
-// 						}
-// 					}},
-// 					{name: 'role-state', args: {
-// 						style: {
-// 							'opacity': '0.0',
-// 						}
-// 					}},
-// 					{name: 'content-state', args: {
-// 						style: {
-// 							'opacity': '1.0',
-// 						}
-// 					}},
-// 				],
-// 			},
-// 			children: [],
-// 		}),
-// 	],
-// });
-//
-// // Render to start
-// UI.getComponent('app').render();
