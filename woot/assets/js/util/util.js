0e537d10289ee54158be8df8e4f19276204ba231
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-28 21:45:57 +0000

    relfile upload form works beautifully

--- woot/assets/js/util/util.js
+++ woot/assets/js/util/util.js
@@ -67,3 +67,11 @@ function makeid() {
 
 	return text;
 }
+
+// extend trunc method for strings
+String.prototype.trunc = function(n, useWordBoundary) {
+	var isTooLong = this.length > n;
+	var s_ = isTooLong ? this.substr(0,n-1) : this;
+	var s_ = (useWordBoundary && isTooLong) ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
+	return  isTooLong ? s_ + '&hellip;' : s_;
+};
