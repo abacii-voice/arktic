f915b0dc418e0fd8e3bfe9e541ef91f58ce2936a
NicholasPiano <nicholas.d.piano@gmail.com>
2016-03-02 01:26:10 +0000

    working on new structure

--- woot/apps/users/static/users/js/account.js
+++ woot/apps/users/static/users/js/account.js
@@ -74,6 +74,177 @@ UI.createGlobalStates('client-state', [
 UI.createApp('hook', [
 	// breadcrumbs
 	UI.createComponent('breadcrumbs', {
+		children: [
+			UI.createComponent('breadcrumbs-client'),
+			UI.createComponent('breadcrumbs-role'),
+			UI.createComponent('breadcrumbs-location'),
+		],
+	}),
+
+	// interfaces
+	// now
+	UI.createComponent('audio-interface', {
+
+	}),
+
+	UI.createComponent('upload-interface', {
+		children: [
+			UI.createComponent('upload-interface-left-panel', {
+				children: [
+					UI.createComponent('new-project-dialogue', {
+						children: [
+							UI.createComponent('project-title'),
+							UI.createComponent('project-name-input'),
+							UI.createComponent('project-deadline-date-picker'),
+							UI.createComponent('project-description-textarea'),
+						],
+					}),
+					UI.createComponent('rel-file-dropzone-wrapper', {
+						children: [
+							UI.createComponent('rel-file-title'),
+							UI.createComponent('rel-file-dropzone', {
+								children: [
+									UI.createComponent('rel-file-dropzone-guide', {
+										children: [
+											UI.createComponent('rel-file-dropzone-guide-title'),
+											UI.createComponent('rel-file-dropzone-guide-table'),
+										],
+									}),
+									UI.createComponent('rel-file-dropzone-warning'),
+								],
+							}),
+							UI.createComponent('rel-file-file-list'),
+						],
+					}),
+				],
+			}),
+			UI.createComponent('upload-interface-right-panel', {
+				UI.createComponent('audio-title'),
+				UI.createComponent('audio-file-dropzone-wrapper', {
+					children: [
+						UI.createComponent('audio-file-dropzone'),
+						UI.createComponent('audio-file-confirmation-panel'),
+					],
+				}),
+				UI.createComponent('upload-confirmation-button'),
+			}),
+		],
+	}),
+
+	UI.createComponent('user-management-interface', {
+		UI.createComponent('user-management-interface-left-panel', {
+			children: [
+				UI.createComponent('user-role-filter'),
+				UI.createComponent('user-list'),
+			],
+		}),
+		UI.createComponent('user-management-interface-right-panel', {
+			children: [
+				UI.createComponent('user-card-wrapper', {
+					children: [
+						UI.createComponent('user-card'),
+						UI.createComponent('new-user-button'),
+						UI.createComponent('new-user-card'),
+					],
+				}),
+			],
+		}),
+	}),
+
+	UI.createComponent('project-interface', {
+		UI.createComponent('project-interface-left-panel', {
+			children: [
+				UI.createComponent('project-list'),
+			],
+		}),
+		UI.createComponent('project-interface-right-panel', {
+			children: [
+				UI.createComponent('project-card-wrapper', {
+					children: [
+						UI.createComponent('project-card'),
+						UI.createComponent('new-project-button'),
+					],
+				}),
+			],
+		}),
+	}),
+
+	UI.createComponent('billing-interface', {
+		UI.createComponent('billing-interface-left-panel', {
+			children: [
+				UI.createComponent('billing-user-search'),
+				UI.createComponent('billing-confirmed-filter'),
+				UI.createComponent('billing-date-list'),
+			],
+		}),
+		UI.createComponent('billing-interface-right-panel', {
+			children: [
+				UI.createComponent('billing-card-wrapper', {
+					children: [
+						UI.createComponent('billing-card'),
+						UI.createComponent('billing-card-confirm-button'),
+					],
+				}),
+			],
+		}),
+	}),
+
+	UI.createComponent('rule-interface', {
+		UI.createComponent('rule-interface-left-panel', {
+			children: [
+				UI.createComponent('rule-search'),
+				UI.createComponent('rule-list'),
+			],
+		}),
+		UI.createComponent('rule-interface-right-panel', {
+			children: [
+				UI.createComponent('rule-card-wrapper', {
+					children: [
+						UI.createComponent('rule-card'),
+						UI.createComponent('new-rule-button'),
+						UI.createComponent('new-rule-card'),
+					],
+				}),
+			],
+		}),
+	}),
+
+	// later
+	UI.createComponent('search-interface', {
+		children: [
+			UI.createComponent('search-bar'),
+			UI.createComponent('search-results'),
+		],
+	}),
+	UI.createComponent('message-interface', {
+
+	}),
+	UI.createComponent('stats-interface', {
+
+	}),
+
+	// sidebars
+	UI.createComponent('client-sidebar-wrapper', {
+		children: [
+			UI.createComponent('client-sidebar'),
+			UI.createComponent('client-back-sidebar'),
+		],
+	}),
+	UI.createComponent('role-sidebar-wrapper', {
+		children: [
+			UI.createComponent('role-sidebar'),
+			UI.createComponent('role-back-sidebar'),
+		],
+	}),
+	UI.createComponent('interface-back-sidebar', {
+
+	}),
+
+]);
+
+UI.createApp('hook', [
+	// breadcrumbs
+	UI.createComponent('breadcrumbs', {
 		template: UI.templates.div,
 		appearance: {
 			style: {
