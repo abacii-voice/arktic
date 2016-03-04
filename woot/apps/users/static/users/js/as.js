b6c5be05e11d66ce287fc736a5f4d7537a9e633a
NicholasPiano <nicholas.d.piano@gmail.com>
2016-03-04 16:01:14 +0000

    Stopping admin signup.
    This is cool, but need to focus on main interface. Will include
    superadmin create-client-sidebar to achieve the same as this page much
    more quickly.

--- woot/apps/users/static/users/js/as.js
+++ woot/apps/users/static/users/js/as.js
@@ -17,26 +17,99 @@ STEPS:
 
 */
 
-// no data to get, I'm a free man. Create dat app.
+// No data to get, I'm a free man. Create dat app.
 UI.createApp('hook', [
 	UI.createComponent('admin-signup-wrapper', {
+		template: UI.template('div', 'ie show cb pn'),
 		children: [
 			UI.createComponent('as-client-dialogue', {
+				template: UI.template('div', 'ie show pn sp br'),
 				children: [
-					UI.createComponent('as-client-name'),
-					UI.createComponent('as-client-type'),
+					UI.createComponent('as-client-details-title-wrapper', {
+						template: UI.template('div', 'ie show rel'),
+						children: [
+							UI.createComponent('as-client-details-title', {
+								template: UI.template('h3', ''),
+								appearance: {
+									html: 'Client details',
+								}
+							}),
+						],
+					}),
+					UI.createComponent('as-client-name', {
+						template: UI.template('input', 'ie show ci rel'),
+						appearance: {
+							properties: {
+								'placeholder': 'Company name',
+							},
+						}
+					}),
+					UI.createComponent('as-client-type-select', {
+						template: UI.template('div', 'ie show pn sp rel'),
+						children: [
+							UI.createComponent('as-client-type-select-contract', {
+								template: UI.template('button', 'ie show bn ba'),
+								appearance: {
+									html: 'Production client',
+								}
+							}),
+							UI.createComponent('as-client-type-select-production', {
+								template: UI.template('button', 'ie show bn ba'),
+								appearance: {
+									html: 'Contract client',
+								}
+							}),
+						],
+					}),
 					UI.createComponent('as-client-contact-phone-number'),
 				],
 			}),
 			UI.createComponent('as-user-dialogue', {
+				template: UI.template('div', 'ie show pn sp'),
 				children: [
-					UI.createComponent('user-name', {
+					UI.createComponent('as-user-details-title-wrapper', {
+						template: UI.template('div', 'ie show rel'),
 						children: [
-							UI.createComponent('un-first-name-input'),
-							UI.createComponent('un-last-name-input'),
+							UI.createComponent('as-user-details-title', {
+								template: UI.template('h3', ''),
+								appearance: {
+									html: 'User details',
+								}
+							}),
 						],
 					}),
-					UI.createComponent('email'),
+					UI.createComponent('user-name', {
+						template: UI.template('div', 'ie show rel'),
+						children: [
+							UI.createComponent('un-first-name-input', {
+								template: UI.template('input', 'ie show ci'),
+								appearance: {
+									properties: {
+										'placeholder': 'First name',
+									},
+								},
+							}),
+							UI.createComponent('un-last-name-input', {
+								template: UI.template('input', 'ie show ci'),
+								appearance: {
+									properties: {
+										'placeholder': 'Last name',
+									},
+								},
+							}),
+						],
+					}),
+					UI.createComponent('email', {
+						template: UI.template('input', 'ie show ci rel'),
+						appearance: {
+							properties: {
+								'placeholder': 'Email',
+							},
+						},
+					}),
+					UI.createComponent('register-description', {
+						// template: 
+					}),
 				],
 			})
 		],
