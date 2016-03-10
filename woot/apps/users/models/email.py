5dd7ab795d6f792fb3a1278731970a69c42dbaa2
NicholasPiano <nicholas.d.piano@gmail.com>
2016-03-10 01:27:02 +0000

    emails and messages

---
+++ woot/apps/users/models/email.py
@@ -0,0 +1,19 @@
+# django
+from django.db import models
+
+# local
+from apps.users.models.role import RoleInstance
+
+### Email models
+class Email(models.Model):
+
+	### Connections
+	from_user = models.ForeignKey(RoleInstance, related_name='emails_from')
+	to_user = models.ForeignKey(RoleInstance, related_name='emails_to')
+
+	### Properties
+	to_email = models.CharField(max_length=255)
+	subject = models.CharField(max_length=255)
+	date_created = models.DateTimeField(auto_now_add=True)
+	text_content = models.TextField()
+	html_content = models.TextField()
