d0ceb933aee03822cefb13fc24d25cbdc43cf4b7
NicholasPiano <nicholas.d.piano@gmail.com>
2016-04-01 13:09:46 +0100

    add projects, branching to upload
    Added alphasort function for roles and clients

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,3 +1,5 @@
+sh scripts/remove_migrations.sh
+sh scripts/make_migrations.sh
 rm db/arktic_db.sqlite3
 python manage.py migrate
 python manage.py temp_create_default_user
--- woot/apps/client/models/project.py
+++ woot/apps/client/models/project.py
@@ -7,11 +7,27 @@ from apps.client.models.client import Client
 # util
 
 ### Project model
-class Project(models.Model):
+class ProjectGroup(models.Model):
 
-	### Connection
+	### Connections
 	production_client = models.ForeignKey(Client, related_name='production_projects')
 	contract_client = models.ForeignKey(Client, related_name='contract_projects')
+
+	### Properties
+	name = models.CharField(max_length=255)
+	description = models.TextField()
+	priority_index = models.PositiveIntegerField(default=0)
+	combined_priority_index = models.PositiveIntegerField(default=0)
+
+	# Statistics
+	due_date = models.DateTimeField(auto_now_add=False)
+	completion_percentage = models.FloatField(default=0.0)
+	redundancy_percentage = models.FloatField(default=0.0)
+
+class Project(models.Model):
+
+	### Connections
+	group = models.ForeignKey(ProjectGroup, related_name='sub_projects')
 
 	### Properties
 	# Identification
