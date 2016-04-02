d609883d9bb7a0395456167747c9be5b622f5d29
NicholasPiano <nicholas.d.piano@gmail.com>
2016-04-02 18:01:05 +0100

    thinking about how to make more complex objects

---
+++ woot/assets/js/ui/components.js
@@ -0,0 +1,29 @@
+var Components = {
+	scrollList: function () {
+		// search field toggle
+		// scroll toggle
+		// infinite scroll loading
+		//
+	},
+
+	searchFilterField: function () {
+		// pop over panel for filter suggestions and search results
+		// keywords and functions
+			// eg. ".." -> function () {} - search for single words
+			// "worker" -> filter by workers
+	},
+
+	roleIndicator: function () {
+		// change based on standard conditions
+		// dispatch requests for each state
+	},
+
+	dataCard: function () {
+		// display data in ordered manner
+		// composed of children and distributor function
+	},
+
+	sidebar: function () {
+		// contains and list and a "back" sidebar from any state it goes to.
+	}
+}
