497a49560d5bef8e230e0a0b461e1b54011b93cc
NicholasPiano <nicholas.d.piano@gmail.com>
2016-04-04 16:52:40 +0100

    adding some cool stuff for later

--- woot/apps/users/static/users/js/account-expt.js
+++ woot/apps/users/static/users/js/account-expt.js
@@ -6,16 +6,64 @@
 // 3. Define component tree
 UI.createApp('hook', [
 	// breadcrumbs
-
+	Components.breadcrumbs(),
 
 	// interfaces
 	UI.createComponent('transcription-interface', {
-		
+		children: [
+
+		],
 	}),
-	UI.createComponent('user-management-interface'),
-	UI.createComponent('project-interface'),
-	UI.createComponent('rule-interface'),
-	UI.createComponent('billing-interface'),
+	UI.createComponent('user-management-interface', {
+		children: [
+			UI.createComponent('umi-button-panel', {
+				UI.createComponent('umi-list-button'),
+				UI.createComponent('umi-new-user-button'),
+				UI.createComponent('umi-settings-button'),
+			}),
+			UI.createComponent('umi-primary-panel', {
+				children: [
+					Components.scrollList('umi-pp-user-list', {
+						title: 'Users',
+						search: {
+							
+						},
+					}),
+					UI.createComponent('umi-pp-user-info'),
+					UI.createComponent('umi-pp-user-confirmed'),
+					UI.createComponent('umi-pp-user-card', {
+						children: [
+							UI.createComponent('umi-pp-uc-control-panel', {
+								children: [
+									UI.createComponent(''),
+								],
+							}),
+							UI.createComponent('umi-pp-uc-moderation-panel'),
+							UI.createComponent('umi-pp-uc-message-panel'),
+							UI.createComponent('umi-pp-uc-stats-panel'),
+						],
+					}),
+				],
+			}),
+			UI.createComponent('umi-new-user-panel'),
+			UI.createComponent('umi-user-confirmed'),
+		],
+	}),
+	UI.createComponent('project-interface', {
+		children: [
+
+		],
+	}),
+	UI.createComponent('rule-interface', {
+		children: [
+
+		],
+	}),
+	UI.createComponent('billing-interface', {
+		children: [
+
+		],
+	}),
 
 	// sidebars
 	Components.sidebar('client-sidebar'),
