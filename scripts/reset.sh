f5a582f93eb71b1b0bc58eefd061e05c2645ec76
NicholasPiano <nicholas.d.piano@gmail.com>
2016-04-20 00:34:39 +0100

    Uploads working well.

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,6 +1,7 @@
 sh scripts/remove_migrations.sh
 sh scripts/make_migrations.sh
 rm db/arktic_db.sqlite3
+rm -rf woot/media/
 python manage.py migrate
 python manage.py temp_create_default_user
 python manage.py runserver
--- woot/apps/client/models/__init__.py
+++ woot/apps/client/models/__init__.py
@@ -1,2 +1,2 @@
 from apps.client.models.client import Client
-from apps.client.models.project import Project
+from apps.client.models.project import Project, Batch, Upload, Fragment
