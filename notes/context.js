c6299c481c73e3c8cb075dd6da0714abbbf0b739
NicholasPiano <nicholas.d.piano@gmail.com>
2016-05-07 16:16:07 +0100

    hmm

--- notes/context.js
+++ notes/context.js
@@ -13,3 +13,13 @@ var get = function (store, path) {
 }
 
 console.log(get(json, 'production_projects.79e4b11f-a7ef-45cd-897e-cd0fe81fa42d.batches.7914f9e1-61c9-444d-9195-d26354c913f0'));
+
+var tr = ajax({
+	url: '/context/',
+	path: 'new_transcription',
+});
+
+tr = {
+	'id': 'sdflksjdflskdfj',
+	'caption': '',
+};
