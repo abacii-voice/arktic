a1c99c53856ba657f9da4fc6f7f0c9edaa85e398
NicholasPiano <nicholas.d.piano@gmail.com>
2016-05-07 19:07:19 +0100

    should be ready for inspection

--- woot/apps/tr/models/check/check.py
+++ woot/apps/tr/models/check/check.py
@@ -2,14 +2,32 @@
 from django.db import models
 
 # local
+from apps.tr.models.client.client import Client
 from apps.tr.models.transcription.caption import Caption
+
+# util
+import uuid
 
 ### Correction classes
 class QualityCheck(models.Model):
 
+	### Connections
+	client = models.ForeignKey(Client, related_name='checks')
+
 	### Properties
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	name = models.CharField(max_length=255)
 	is_automatic = models.BooleanField(default=True)
+
+	### Methods
+	# data
+	def data(self):
+		data = {
+			'name': self.name,
+			'is_automatic': self.is_automatic,
+		}
+
+		return data
 
 class QualityCheckInstance(models.Model):
 
@@ -18,14 +36,16 @@ class QualityCheckInstance(models.Model):
 	caption = models.ForeignKey(Caption, related_name='tests')
 
 	### Properties
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	is_successful = models.BooleanField(default=True)
 	date_created = models.DateTimeField(auto_now_add=True)
 
 	### Methods
 	def data(self):
-		data = {
+		data = self.parent.data()
+		data.update({
 			'is_successful': self.is_successful,
 			'date_created': str(self.date_created),
-		}
+		})
 
 		return data
--- woot/apps/tr/tests.py
+++ woot/apps/tr/tests.py
@@ -5,19 +5,6 @@ from django.core.files import File
 # local
 from apps.users.models import User
 from apps.tr.models.client.client import Client
-from apps.tr.models.client.project import Project, Batch, Upload, Fragment
-from apps.tr.models.role.action import Action
-from apps.tr.models.role.role import Role, Threshold
-from apps.tr.models.role.stat import Stat, StatInstance
-from apps.tr.models.check.check import QualityCheck, QualityCheckInstance
-from apps.tr.models.transcription.caption import Caption
-from apps.tr.models.transcription.dictionary import Dictionary, UserDictionary
-from apps.tr.models.transcription.flag import Flag, FlagInstance
-from apps.tr.models.transcription.grammar import Grammar
-from apps.tr.models.transcription.moderation import Moderation
-from apps.tr.models.transcription.rule import Rule, RuleInstance
-from apps.tr.models.transcription.token import Token, TokenInstance
-from apps.tr.models.transcription.transcription import Transcription
 from apps.tr.models.transcription.utterance import Utterance
 
 # util
@@ -73,11 +60,15 @@ class ContextTestCase(TestCase):
 		token_instance = token.instances.create(caption=caption)
 
 		# flag, flagInstance
-		flag = Flag.objects.create(name='unsure')
+		flag = contract_client.flags.create(name='unsure')
 		flag_instance = flag.instances.create(caption=caption)
 
 		# moderation
 		moderation = moderator.moderations.create(caption=caption)
+
+		# quality
+		quality_check = contract_client.checks.create(name='automatic_capital_check')
+		quality_check_instance = quality_check.instances.create(caption=caption)
 
 		# rule
 		rule = project.rules.create(client=contract_client, name='Capital Letters', description='Use only lower case letters.')
@@ -87,5 +78,5 @@ class ContextTestCase(TestCase):
 		production_client = Client.objects.get(name='TestProductionClient')
 		print(json.dumps(production_client.data(), indent=2, sort_keys=True))
 
-		# contract_client = Client.objects.get(name='TestContractClient')
-		# print(json.dumps(contract_client.data(), indent=2, sort_keys=True))
+		contract_client = Client.objects.get(name='TestContractClient')
+		print(json.dumps(contract_client.data(), indent=2, sort_keys=True))
