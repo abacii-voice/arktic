9f6307bd3856b52596829e0bdc212ff84867a308
NicholasPiano <nicholas.d.piano@gmail.com>
2016-05-10 16:11:40 +0100

    testing context

--- woot/apps/tr/requests/context.py
+++ woot/apps/tr/requests/context.py
@@ -1,14 +1,16 @@
 # django
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.conf import settings
 
 # local
+from apps.tr.access import access, process_request
 
 # util
 
 
 ### Context
-def context(request):
-	
+def context(request, path):
+	user, permission, data, verified = process_request(request)
+	return JsonResponse({'done': True})
