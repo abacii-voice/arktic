42da43ae3b338a6f99a0e205fc3c9b6749db08a7
NicholasPiano <nicholas.d.piano@gmail.com>
2016-05-14 20:03:31 +0100

    promises working so far

--- woot/apps/tr/static/tr/js/test_account.js
+++ woot/apps/tr/static/tr/js/test_account.js
@@ -2,73 +2,98 @@
 
 var Test = {
 	tests: {
-		test_registry: function () {
-			// set promise
-			function setData () {
-				return new Promise(function (resolve, reject) {
-					// set stuff
-					UI.globalState = 'state';
-					UI.components = {
-						'id1': {
-							'id': 'id1',
+		test_registry_trigger: function () {
+
+			var gtf = function (message) {
+				return function (resolve, reject) {
+					setTimeout(function () {
+						console.log(message);
+						resolve();
+					}, 1000);
+				}
+			}
+
+			var test_structure = {
+				'top1': {
+					'bottom': {
+						'reg': {
+							'b1': gtf('1'),
 						},
-						'id2': {
-							'id': 'id2',
+					},
+					'reg': {
+						'b2': gtf('2'),
+					},
+				},
+				'top2': {
+					'bottom': {
+						'reg': {
+							'b3': gtf('3'),
+						},
+					},
+					'reg': {
+						'b4': gtf('4'),
+					},
+				},
+				'top3': {
+					'bottom': {
+						'reg': {
+							'b5': gtf('5'),
+						},
+					},
+					'reg': {
+						'b6': gtf('6'),
+					},
+				},
+			};
+
+			var get = function (structure, path) {
+				return new Promise(function (resolve, reject) {
+					context_path = path.split('.');
+					sub = structure;
+					for (i=0; i<context_path.length; i++) {
+						sub = sub[context_path[i]];
+						if (sub === undefined) {
+							break;
 						}
-					};
-					Registry.registry = {
-						'state': {
-							'clients': {
-								'registered': {
-									'id1': function (component, data) {
-										console.log('state.clients.id1', data);
-									}
-								},
-								'6f56a306-cfa9-4557-bec9-f65bd2de67e0': {
-									'registered': {
-										'id2': function (component, data) {
-											console.log('state.clients.6f56a306-cfa9-4557-bec9-f65bd2de67e0.id2', data);
-										}
-									},
+					}
+
+					resolve(sub);
+				});
+			}
+
+			// console.log(get(test_structure, 'top1.bottom.reg'));
+
+			var recursivePromise = function (parent, level) {
+				return new Promise(function (resolve, reject) {
+					parent = parent !== undefined ? parent : '';
+					level = level !== undefined ? level : test_structure;
+
+					if ('reg' in level) {
+						return Promise.all(Object.keys(level.reg).map(function (key) {
+							return new Promise(level.reg[key]);
+						})).then(function () {
+							return Promise.all(Object.keys(level).map(function (key) {
+								if (key !== 'reg') {
+									var get = '{parent}{dot}{path}'.format({parent: parent, dot: (parent !== '' ? '.' : ''), path: key});
+									return recursivePromise(get, level[key]);
 								}
-							},
-						},
-					};
-					resolve();
+							}));
+						});
+					} else {
+						return Promise.all(Object.keys(level).map(function (key) {
+							var get = '{parent}{dot}{path}'.format({parent: parent, dot: (parent !== '' ? '.' : ''), path: key});
+							return recursivePromise(get, level[key]);
+						}));
+					}
+
+					resolve(parent, level);
 				});
 			}
 
-			// run promise
-			function runPromise () {
-				return Registry.trigger();
-			}
-
-			// assert promise
-			function assertPromise () {
-				return new Promise(function (resolve, reject) {
-					// assert stuff
-					resolve();
-				});
-			}
-
-			// delete promise
-			function deletePromise () {
-				return new Promise(function (resolve, reject) {
-					// delete stuff
-					Context.context = {};
-					resolve();
-				});
-			}
-
-			setData().then(function () {
-				return runPromise();
-			}).then(function () {
-				return assertPromise();
-			}).then(function () {
-				return deletePromise();
-			}).then(function () {
+			recursivePromise().then(function () {
 				console.log('done');
 			});
+
 		}
 	},
 
