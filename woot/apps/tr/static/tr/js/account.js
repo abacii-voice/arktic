00ab5a8d99550686e89143e4939e2bc394e0a120
NicholasPiano <nicholas.d.piano@gmail.com>
2016-05-17 23:19:57 +0100

    branching for component building

--- woot/apps/tr/static/tr/js/account.js
+++ woot/apps/tr/static/tr/js/account.js
@@ -2,10 +2,27 @@
 // 1. define component tree
 UI.createApp('hook', [
 
-	// interfaces
+	// COMPONENT TESTING
+	// autocomplete field
+	UI.createComponent('test-autocomplete-wrapper', {
+		template: UI.template('div', ''),
+		appearance: {
+			style: {
+				'position': 'relative',
+				'height': '600px',
+				'width': '300px',
+				'border': '2px solid #ccc',
+				'left': '50%',
+				'top': '50%',
+				'transform': 'translate(-50%, -50%)',
+			},
+		},
+		children: [
+			Components.scroll('test-scroll', {
 
-	// sidebars
-
+			}),
+		],
+	}),
 ]);
 
 UI.renderApp('client-state');
