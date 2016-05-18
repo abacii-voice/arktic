9ff73847a29a52d0ec82ac8cffe2241b0cdcdc42
NicholasPiano <nicholas.d.piano@gmail.com>
2016-05-18 14:06:31 +0100

    trying out scroll component

--- woot/apps/tr/static/tr/js/account.js
+++ woot/apps/tr/static/tr/js/account.js
@@ -11,7 +11,6 @@ UI.createApp('hook', [
 				'position': 'relative',
 				'height': '600px',
 				'width': '300px',
-				'border': '2px solid #ccc',
 				'left': '50%',
 				'top': '50%',
 				'transform': 'translate(-50%, -50%)',
@@ -19,10 +18,96 @@ UI.createApp('hook', [
 		},
 		children: [
 			Components.scroll('test-scroll', {
+				appearance: {
+					style: {
+						'height': '100%',
+						'width': '100%',
+					}
+				},
+				options: {
+					title: 'Title',
+					scroll: true,
+					sort: true,
+					search: {
 
+						// This defines what is being searched for, i.e. paths in context.
+						target: function () {
+
+						},
+
+						// This defines how to respond to a query.
+						query: function (query) {
+							// 'query' is the text entered by the user after having been cleaned by the standard function
+							return function (resolve, reject) {
+								// This returns a function ready for a promise to use in the response chain.
+
+								resolve();
+							}
+						},
+
+						// This filters a search in progress by the currently imposed filter.
+						filter: function (query) {
+							return function (resolve, reject) {
+								// This returns a function ready for a promise to use in the response chain.
+
+								resolve();
+							}
+						},
+					},
+
+					// Define a way of display individual list items
+					display: function (id, data) {
+						// 'data' is a single unit of the full dataset to be included.
+						var unit = UI.createComponent('{id}-{key}'.format({id: id, key: data.key}), {
+							root: id,
+							template: UI.template('div', 'ie button'),
+							appearance: {
+								style: {
+
+								},
+							},
+							children: [
+								UI.createComponent('{id}-{key}-main'.format({id: id, key: data.key}), {
+									template: UI.template('span', 'ie'),
+								}),
+								UI.createComponent('{id}-{key}-tag'.format({id: id, key: data.key}), {
+									template: UI.template('span', 'ie'),
+								}),
+								UI.createComponent('{id}-{key}-index'.format({id: id, key: data.key}), {
+									template: UI.template('span', 'ie'),
+								}),
+							],
+						});
+
+						unit.render();
+					}
+				},
 			}),
 		],
 	}),
 ]);
 
 UI.renderApp('client-state');
+
+
+// Search would go something like:
+// enterText (text) {
+// 	return this.clean(text).then(this.query).then(this.filter).then(function (listOfSearchResults) {
+// 		listOfSearchResults.forEach(function (searchResult) {
+//
+// 			searchResult = {
+// 				body: 'Piano, Nicholas',
+// 				tag: 'user',
+// 				index: '#1',
+// 			}
+//
+// 			if () {
+// 				result = UI.createComponent('id based on id or whatever', {
+// 					// stuff
+// 				});
+//
+// 				result.render();
+// 			}
+// 		});
+// 	});
+// }
