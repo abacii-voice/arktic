9df559f5a1bbbde2c55f800f5f0648dd97dc0a33
NicholasPiano <nicholas.d.piano@gmail.com>
2016-05-18 14:06:31 +0100

    trying out scroll component

--- woot/assets/js/ui/components.js
+++ woot/assets/js/ui/components.js
@@ -15,25 +15,107 @@ var Components = {
 	scroll: function (id, args) {
 
 		// arg setup and initialisation
+		// - if no title is given, leave no room for a title.
+		// - if no search is given, leave no room for an input.
+		// - looking for final variable 'listHeight'
+		var listHeight = '100%', offset = 0, titleText, search;
+		if (args.options !== undefined) {
+			// title
+			if (args.options.title !== undefined) {
+				titleText = args.options.title;
+				offset += 22;
+			}
+
+			// search
+			if (args.options.search !== undefined) {
+				search = args.options.search;
+				offset += 40;
+			}
+
+			// listHeight
+			listHeight = 'calc(100% - {offset}px)'.format({offset: offset});
+		}
 
 		// create child components
-		var input = UI.createComponent('{id}-'.format({id: id}), {});
-		var loadingIcon = UI.createComponent('{id}-'.format({id: id}), {});
-		var filterPanel = UI.createComponent('{id}-'.format({id: id}), {});
-		var list = UI.createComponent('{id}-'.format({id: id}), {}); // in future, allow this to be bound to another element.
-		var info = UI.createComponent('{id}-'.format({id: id}), {});
-		var title = UI.createComponent('{id}-'.format({id: id}), {});
+		var title, input, loadingIcon, list, filter;
+		if (titleText !== undefined) {
+			title = UI.createComponent('{id}-title'.format({id: id}), {
+				template: UI.template('h4', 'ie title'),
+				appearance: {
+					style: {
+						'width': '100%',
+						'height': '22px',
+					},
+					html: titleText,
+				},
+			});
+		}
 
-		// styling
-		// - The entire device should occupy 100% of the width and height of its container, or the base component.
-		// - space needs to be reserved for the title and input field.
+		if (search !== undefined) {
+			input = UI.createComponent('{id}-search'.format({id: id}), {
+				template: UI.template('input', 'ie input'),
+				appearance: {
+					style: {
+						'width': '100%',
+						'height': '40px',
+					}
+				},
+				bindings: {
+					'focus': {
+						'fn': function (_this) {
+							list.model().hide();
+							filter.model().show();
+						}
+					},
+					'blur': {
+						'fn': function (_this) {
+							list.model().show();
+							filter.model().hide();
+						}
+					}
+				},
+			});
+
+			filter = UI.createComponent('{id}-filter'.format({id: id}), {
+				template: UI.template('div', 'ie hidden'),
+				appearance: {
+					style: {
+						'width': '100%',
+						'height': listHeight,
+					},
+				},
+			});
+		}
+
+		var loadingIcon = UI.createComponent('{id}-loading-icon'.format({id: id}), {
+			template: UI.templates.loadingIcon,
+		});
+		loadingIcon.fade = function () {
+			loadingIcon.model().fadeOut();
+		};
+		var list = UI.createComponent('{id}-list'.format({id: id}), {
+			// in future, allow this to be bound to another element.
+			template: UI.template('div', 'ie'),
+			appearance: {
+				style: {
+					'width': '100%',
+					'height': listHeight,
+				},
+			},
+			children: [
+				loadingIcon,
+			],
+		});
 
 		// create base component
 		var base = UI.createComponent(id, {
 			template: UI.template('div', ''),
 			appearance: args.appearance,
 			children: [
-
+				title,
+				input,
+				list,
+				filter,
 			],
 		});
 
