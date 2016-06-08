d91debb26dbb40b2420b2130070e121f5868ff7c
NicholasPiano <nicholas.d.piano@gmail.com>
2016-06-08 19:02:02 +0100

    audio working pretty well with multiple tracks

--- woot/assets/js/ui/ui.js
+++ woot/assets/js/ui/ui.js
@@ -936,20 +936,19 @@ var Registry = {
 }
 
 var Request = {
-	get: function (url, args) {
-		return Permission.permit(args).then(function (data) {
-			var ajax_data = {
-				type: 'post',
-				data: data,
-				url: '/command/{url}/'.format({url: url}),
-				error: function (xhr, ajaxOptions, thrownError) {
-					if (xhr.status === 404 || xhr.status === 0) {
-						Request.get(url, args);
-					}
-				}
-			}
-
-			return $.ajax(ajax_data);
+	load_audio: function (transcriptionId) {
+		return Permission.permit({id: transcriptionId}).then(function (data) {
+			return new Promise(function(resolve, reject) {
+				request = new XMLHttpRequest();
+				request.open('POST', '/command/load_audio/', true);
+				request.responseType = 'arraybuffer';
+				request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+				request.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
+				request.addEventListener('load', function (event) {
+					resolve(event.target.response);
+				}, false);
+				request.send(data);
+			});
 		});
-	}
+	},
 }
