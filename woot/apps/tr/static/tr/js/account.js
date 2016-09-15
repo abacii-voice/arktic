d185167ae215026c94c8f26cd87d7a340515aa75
NicholasPiano <nicholas.d.piano@gmail.com>
2016-09-15 18:59:06 -0500

    Asynchronous style animations?
    Could I build up styles from different sources that are finally called
    by the setAppearance method at the end? This is instead of chaining
    them.

--- woot/apps/tr/static/tr/js/account.js
+++ woot/apps/tr/static/tr/js/account.js
@@ -6,15 +6,16 @@ UI.app('hook', [
 			corner: 5,
 		},
 		colour: {
-			
+
 		},
 	}),
 ]).then (function (app) {
 	return app.render();
 }).then(function () {
 	return Promise.all([
-
+		Active.set('client', '05ff19e5-446f-43cb-ae3e-8b2a3f7d5a40'),
+		Active.set('role', 'a48408d1-81cc-44da-927e-40d081f10e0b'),
 	]);
 }).then(function () {
-	return UI.changeState('client-state');
+	return UI.changeState('transcription-state');
 });
