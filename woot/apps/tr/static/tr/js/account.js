d9f5a5620c26b3f36129dc9ff9ea3f25b5eb61a8
NicholasPiano <nicholas.d.piano@gmail.com>
2016-09-15 19:11:52 -0500

    Has the transcription token thing broken?
    Need to check and see. I haven’t been to backendland for a long time.

--- woot/apps/tr/static/tr/js/account.js
+++ woot/apps/tr/static/tr/js/account.js
@@ -15,6 +15,7 @@ UI.app('hook', [
 	return Promise.all([
 		Active.set('client', '05ff19e5-446f-43cb-ae3e-8b2a3f7d5a40'),
 		Active.set('role', 'a48408d1-81cc-44da-927e-40d081f10e0b'),
+		Permission.set('a48408d1-81cc-44da-927e-40d081f10e0b'),
 	]);
 }).then(function () {
 	return UI.changeState('transcription-state');
