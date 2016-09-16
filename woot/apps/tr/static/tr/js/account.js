a03a17c1ea500935b272c3af5a55ffea589128c3
NicholasPiano <nicholas.d.piano@gmail.com>
2016-09-15 19:45:10 -0500

    having problems with transcription token apparently

--- woot/apps/tr/static/tr/js/account.js
+++ woot/apps/tr/static/tr/js/account.js
@@ -13,10 +13,10 @@ UI.app('hook', [
 	return app.render();
 }).then(function () {
 	return Promise.all([
-		Active.set('client', '05ff19e5-446f-43cb-ae3e-8b2a3f7d5a40'),
-		Active.set('role', 'a48408d1-81cc-44da-927e-40d081f10e0b'),
-		Permission.set('a48408d1-81cc-44da-927e-40d081f10e0b'),
+		// Active.set('client', '05ff19e5-446f-43cb-ae3e-8b2a3f7d5a40'),
+		// Active.set('role', 'a48408d1-81cc-44da-927e-40d081f10e0b'),
+		// Permission.set('a48408d1-81cc-44da-927e-40d081f10e0b'),
 	]);
 }).then(function () {
-	return UI.changeState('transcription-state');
+	return UI.changeState('client-state');
 });
