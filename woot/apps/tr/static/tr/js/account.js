53fe2224bb54b7cf51a1e675d0d7ff97880862d7
NicholasPiano <nicholas.d.piano@gmail.com>
2016-09-26 20:14:44 +0100

    finished updateData method

--- woot/apps/tr/static/tr/js/account.js
+++ woot/apps/tr/static/tr/js/account.js
@@ -15,9 +15,6 @@ UI.app('hook', [
 	return Promise.all([
 		Context.get('clients'),
 		Context.get('user'),
-		// Active.set('client', '030c6bd1-e37c-4af7-a6d7-89d05bf3bc57'),
-		// Active.set('role', '5e84c20d-b52c-4acc-af43-8c1ad7091ea8'),
-		// Permission.set('5e84c20d-b52c-4acc-af43-8c1ad7091ea8'),
 	]);
 }).then(function () {
 	return UI.changeState('client-state');
