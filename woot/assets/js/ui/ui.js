983daf4d29a6355f3d9e998cc8501cdd7627deac
NicholasPiano <nicholas.d.piano@gmail.com>
2016-10-16 13:09:09 +0100

    attempted to move setChildren out of loop

--- woot/assets/js/ui/ui.js
+++ woot/assets/js/ui/ui.js
@@ -373,7 +373,6 @@ var UI = {
 		}
 		this.setChildren = function (children) {
 			var _this = this;
-			console.log(_this.id);
 			_this.children = (_this.children || []);
 			if (children !== undefined) {
 				return Promise.ordered(children.map(function (child) {
