a37cdc6e7229d56c538222fce3f1e47157eb467f
NicholasPiano <nicholas.d.piano@gmail.com>
2016-10-20 21:28:21 +0100

    new swipe at caption field
    This time I’m trying it with a really close look at what Nodes can do.

--- woot/apps/tr/static/tr/js/account.js
+++ woot/apps/tr/static/tr/js/account.js
@@ -17,7 +17,14 @@ UI.app('hook', [
 		Context.get('user'),
 	]);
 }).then(function () {
-	return UI.changeState('client-state');
+	return Promise.all([
+		Active.set('client', 'ebb9e77a-0ed1-412f-9579-83242186d63d'),
+		Active.set('role', 'aecca9b9-09c5-4fee-ab40-bcc287142e3e'),
+		Active.set('project', 'ee9dc4ca-34ad-4491-89d8-6f86625f60d6'),
+		Permission.set('aecca9b9-09c5-4fee-ab40-bcc287142e3e'),
+	]).then(function () {
+		return UI.changeState('transcription-state');
+	});
 }).catch(function (error) {
 	console.log(error);
 });
