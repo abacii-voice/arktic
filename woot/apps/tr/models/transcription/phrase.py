32f3574f820c30ca7c49e492bea397e93d6bc54f
NicholasPiano <nicholas.d.piano@gmail.com>
2016-11-16 16:39:17 +0000

    ghost

---
+++ woot/apps/tr/models/transcription/phrase.py
@@ -0,0 +1,15 @@
+
+# On Phrases:
+# I want Phrases to be another way of storing tokens. Not locked down to a caption.
+# That way, users can store phrases that they see a lot.
+
+# On Ghost Units:
+# I think the caption units need to be rendered in a very similar manner to searchableList.
+# A predetermined number (10) of units will be rendered with nothing in them.
+# Then, they can be updated, but never removed.
+# Three types:
+# 1. Word
+# 2. Tag
+# 3. Ghost
+
+# Ghost units are dimmed out 
