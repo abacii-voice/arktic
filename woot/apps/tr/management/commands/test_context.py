ab3a15c9b7906137d0140b69aa5e617a7749b226
NicholasPiano <nicholas.d.piano@gmail.com>
2016-11-22 13:30:16 +0000

    will not automatically get new token on page reload.

--- woot/apps/tr/management/commands/test_context.py
+++ woot/apps/tr/management/commands/test_context.py
@@ -28,7 +28,7 @@ class Command(BaseCommand):
 		# path = 'clients.{client}.projects.{project}.transcriptions'.format(client=client.id, project=project.id)
 		# fltr = {'tokens': {'content__startswith': 'p'}}
 		# path = 'clients.ef10b3e8-d1b4-4f0b-bc42-2e7688088aee.projects.6bd2a2b4-4799-4a39-b1b9-15114c4460eb.dictionary.tokens'
-		path = 'user.clients.30911184-da94-443f-bc7a-a580725c1fdd.roles.551a3ccf-8eb2-49b2-abd5-ef1f0d821880.active_transcription_token.fragments'
+		path = 'user.clients'
 		# path = 'clients'
 
 		# request data using path
