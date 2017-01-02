11c0607895f59020ff642e0e6451a160867b4520
NicholasPiano <nicholas.d.piano@gmail.com>
2017-01-02 14:18:42 +0000

    ready to branch for 1.0

--- woot/apps/tr/static/tr/js/account.application.js
+++ woot/apps/tr/static/tr/js/account.application.js
@@ -1,1 +1,50 @@
-replace the unified interface
+var AccountApplication = function (id, args) {
+	return Promise.all([
+		// base
+		UI.createComponent('{id}-base'.format({id: id}), {
+			template: UI.template('div', 'ie'),
+			appearance: {
+				style: {
+					'height': '100%',
+				},
+			},
+		}),
+
+		// control interface
+		AccountInterfaces.controlInterface('control-interface', {
+			interface: args.interface,
+		}),
+
+		// transcription interface
+		AccountInterfaces.transcriptionInterface('transcription-interface', {
+			interface: args.interface,
+		}),
+
+	]).then(function (components) {
+		// unpack components
+		var [
+			base,
+			controlInterface,
+			transcriptionInterface,
+		] = components;
+
+		// ASSOCIATE
+		// key bindings and other
+		// TRANSCRIPTION INTERFACE
+		controlInterface.name = 'controlInterface';
+		transcriptionInterface.name = 'transcriptionInterface';
+
+		// complete promises
+		return Promise.all([
+
+		]).then(function () {
+			// base children
+			return base.setChildren([
+				controlInterface,
+				transcriptionInterface,
+			]);
+		}).then(function () {
+			return base;
+		});
+	});
+}
---
+++ woot/apps/tr/static/tr/js/account.interfaces/account.interfaces.moderationInterface.js
@@ -0,0 +1,4 @@
+var AccountInterfaces = (AccountInterfaces || {});
+AccountInterfaces.moderationInterface = function (id, args) {
+
+}
