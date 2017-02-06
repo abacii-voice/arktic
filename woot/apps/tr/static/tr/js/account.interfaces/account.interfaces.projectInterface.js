b4dcd8f81acb354564bd9c1cf72e267160a6b2f7
NicholasPiano <nicholas.d.piano@gmail.com>
2017-02-06 21:05:54 +0000

    starting project interface

--- woot/apps/tr/static/tr/js/account.interfaces/account.interfaces.projectInterface.js
+++ woot/apps/tr/static/tr/js/account.interfaces/account.interfaces.projectInterface.js
@@ -1,4 +1,21 @@
 var AccountInterfaces = (AccountInterfaces || {});
 AccountInterfaces.projectInterface = function (id, args) {
+	return Promise.all([
 
+		// base
+		UI.createComponent(),
+
+	]).then(function (components) {
+		var [
+			base,
+		] = components;
+
+		return Promise.all([
+			
+		]).then(function () {
+			return base.setChildren([]);
+		}).then(function () {
+			return base;
+		});
+	});
 }
