abb5cf554f9bc1320413d04df202b2e83ce35151
NicholasPiano <nicholas.d.piano@gmail.com>
2017-02-17 18:10:36 +0000

    moving contents of drop zone into a temporary buffer for consolidation

--- woot/apps/tr/static/tr/js/account.components/account.components.uploadController.js
+++ woot/apps/tr/static/tr/js/account.components/account.components.uploadController.js
@@ -9,19 +9,43 @@ AccountComponents.uploadController = function () {
 				any: function () {
 					return base.upload.error.no_zip_or_folder || base.upload.error.no_relfile || base.upload.error.relfile_duplicates || base.upload.error.no_caption || base.error.upload.server_duplicates;
 				},
-				no_zip_or_folder: false,
-				no_relfile: false,
-				relfile_duplicates: false,
-				no_caption: false,
-				server_duplicates: false,
+				noZipOrFolder: false,
+				noRelfile: false,
+				relfileDuplicates: false,
+				noCaption: false,
+				serverDuplicatesWithinBatch: false,
 			},
 			buffer: {
 				id: undefined,
-				addAudio: function () {
-
-				},
-				relfile: {},
 				audio: {},
+				relfile: {
+					entries: {},
+				},
+				addAudio: function (audioFile) {
+					let filename = Util.basename(audioFile.name);
+					if (filename in base.upload.buffer.relfile.entries) {
+						audioFile.caption = base.upload.buffer.relfile.entries[filename].caption;
+					}
+					base.upload.buffer.audio[filename] = audioFile;
+					base.upload.buffer.check();
+				},
+				addRelfile: function (relfileContent) {
+					relfileContent.split('\n').forEach(function (line) {
+						let [filename, caption] = line.split(',');
+						filename = Util.basename(filename);
+						if (filename.contains('.wav')) {
+							if (filename in base.upload.buffer.relfile.entries) {
+								base.upload.buffer.relfile.entries[filename].isDuplicate = true;
+							} else {
+								base.upload.buffer.relfile.entries[filename] = {caption: caption};
+							}
+						}
+					});
+					base.upload.buffer.check();
+				},
+				check: function () {
+					// check for errors and accept or reject, then push
+				},
 				push: function () {
 					// reset buffer
 					// reset id
@@ -31,95 +55,68 @@ AccountComponents.uploadController = function () {
 			unpack: function (file) {
 				// sort by file type
 				if (file.type === 'audio/wav') {
-
-				} else if (file.type === 'text/csv') {
+					// 1. add to buffer.audio
+					base.upload.buffer.addAudio(file);
+				} else if (file.type === 'text/csv' || file.name.contains('.csv')) {
+					// 2. add to buffer.relfile
+					var reader = new FileReader();
+					reader.onload = function(e) {
+						base.upload.buffer.addRelfile(e.target.result);
+					}
+					reader.readAsBinaryString(file);
 
 				} else if (file.type === 'application/zip') {
+					// 3. create zip object and unzip
+					var zipReader = new FileReader();
+					var zip = new JSZip();
+					zipReader.onload = function (e) {
+						// load zip
+						zip.load(e.target.result);
 
+						// 1. extract relfile
+						var relfileData = Object.keys(zip.files).filter(function (key) {
+							return key.contains('.csv');
+						}).map(function (key) {
+							return zip.file(key);
+						})[0];
+						if (relfileData) {
+							base.upload.buffer.addRelfile(zip.file(relfileData.name).asText());
+						}
+
+						// 2. extract audio files
+						Object.keys(zip.files).filter(function (key) {
+							return key.contains('.wav');
+						}).forEach(function (key) {
+							base.upload.buffer.addAudio(zip.file(key));
+						});
+
+						console.log(Object.keys(base.upload.buffer.audio).length);
+					}
 				} else {
-
+					console.log('no');
 				}
 
 				// check for complete
 
 
-				// try reading file
-				var zipReader = new FileReader();
-				var zip = new JSZip();
-				zipReader.onload = function(e) {
-					var contents = e.target.result;
-					zip.load(contents);
-
-					// 1. get relfile from zip
-					var relfileData = Object.keys(zip.files).filter(function (key) {
-						return key.contains('.csv');
-					}).map(function (key) {
-						return zip.file(key);
-					})[0];
-					base.error.relfile = (base.error.relfile || relfileData === undefined);
-
-					// 2. open and get list of filenames and their captions
-					if (!base.error.any()) {
-						var relfileContent = zip.file(relfileData.name).asText();
-						var relfileCaptions = {};
-						relfileContent.split('\n').forEach(function (line) {
-							let [filename, caption] = line.split(',');
-							filename = Util.basename(filename);
-							if (filename.contains('.wav')) {
-								if (filename in relfileCaptions) {
-									if (!base.error.relfile_duplicates) {
-										base.error.relfile_duplicates = true;
-									}
-									relfileCaptions[filename].isDuplicate = true;
-								} else {
-									relfileCaptions[filename] = {caption: caption};
-								}
-							}
-						});
-					}
-
-					// 3. place files to be uploaded in Context under contract_client > new_uploads
-					if (!base.error.any()) {
-						var audioFiles = Object.keys(zip.files).filter(function (key) {
-							return key.contains('.wav');
-						}).map(function (key) {
-							return zip.file(key);
-						});
-
-						var audioDataInstances = {};
-						audioFiles.forEach(function (audioFile) {
-							let filename = Util.basename(audioFile.name);
-							if (filename in relfileCaptions) {
-								audioFile.caption = relfileCaptions[filename].caption;
-							} else {
-								audioFile.caption = '';
-								audioFile.noCaption = true;
-								base.error.no_caption = true;
-							}
-							audioDataInstances[filename] = audioFile;
-						});
-					}
-
-					// get relevant data
-					return Promise.all([
-						Active.get('client'),
-						Active.get('contract_client'),
-					]).then(function (results) {
-						if (!base.error.any()) {
-							var [client_id, contract_client] = results;
-							return Context.set('clients.{client_id}.contract_clients.{contract_client_id}.projects.{project_id}.uploads.{upload_id}'.format({
-								client_id: client_id,
-								contract_client_id: contract_client.id,
-								project_id: contract_client.project,
-								upload_id: id,
-							}), {audioData: audioDataInstances});
-						}
-					}).then(function () {
-						// 4. trigger state to prompt display lists to draw from Context
-						return base.triggerState();
-					});
-				}
-				zipReader.readAsBinaryString(file);
+				// get relevant data
+				// return Promise.all([
+				// 	Active.get('client'),
+				// 	Active.get('contract_client'),
+				// ]).then(function (results) {
+				// 	if (!base.error.any()) {
+				// 		var [client_id, contract_client] = results;
+				// 		return Context.set('clients.{client_id}.contract_clients.{contract_client_id}.projects.{project_id}.uploads.{upload_id}'.format({
+				// 			client_id: client_id,
+				// 			contract_client_id: contract_client.id,
+				// 			project_id: contract_client.project,
+				// 			upload_id: id,
+				// 		}), {audioData: audioDataInstances});
+				// 	}
+				// }).then(function () {
+				// 	// 4. trigger state to prompt display lists to draw from Context
+				// 	return base.triggerState();
+				// });
 			},
 			preUpload: function () {
 				// send list of files and hash
