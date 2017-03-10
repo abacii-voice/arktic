bff97d0f0ba0d56570de0f76f8a1524c5c27a2a4
NicholasPiano <nicholas.d.piano@gmail.com>
2017-03-10 15:22:48 +0000

    oops

--- scripts/reset_staging.sh
+++ scripts/reset_staging.sh
@@ -1,6 +1,5 @@
 sh scripts/remove_migrations.sh
-sh scripts/make_migrations.sh
-rm db/arktic_db.sqlite3
+sh scripts/make_migrations_staging.sh
 rm -rf woot/media/
 python manage.py migrate --settings="woot.settings.staging"
 python manage.py temp_create_test_data --settings="woot.settings.staging"
