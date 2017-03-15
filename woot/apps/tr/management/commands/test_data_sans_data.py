8f5ef1bcef16a81152df0521b0149716aa4d6c74
NicholasPiano <nicholas.d.piano@gmail.com>
2017-03-15 18:53:04 +0000

    new test data

---
+++ woot/apps/tr/management/commands/test_data_sans_data.py
@@ -0,0 +1,46 @@
+# django
+from django.core.management.base import BaseCommand, CommandError
+from django.core.files import File
+from django.conf import settings
+
+# local
+from apps.tr.models.client.client import Client
+from apps.users.models import User
+
+# util
+import os
+from os.path import join, exists, isdir
+
+### Command: create default user
+class Command(BaseCommand):
+
+	def handle(self, *args, **options):
+
+		### CLIENTS, USERS, ROLES
+
+		# create clients
+		production_client, production_client_created = Client.objects.get_or_create(name='TestProductionClient', is_production=True)
+		contract_client, contract_client_created = Client.objects.get_or_create(name='TestContractClient', is_production=False)
+
+		# create user
+		user, user_created = User.objects.get_or_create(email='n@a.com', first_name='Nicholas', last_name='Piano')
+		user.set_password('mach')
+
+		# create roles
+		production_admin_role = production_client.add_admin(user)
+		contract_admin_role = contract_client.add_admin(user)
+		moderator_role = production_client.add_moderator(user)
+		worker_role = production_client.add_worker(user)
+
+		# save
+		user.save()
+
+		# second user
+		user2, user2_created = User.objects.get_or_create(email='s@a.com', first_name='StJohn', last_name='Piano')
+		user2.set_password('mach')
+
+		# create roles
+		worker_role2 = production_client.add_worker(user2)
+
+		# save
+		user2.save()
