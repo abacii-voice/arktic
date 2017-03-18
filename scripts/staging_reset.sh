988cf570e5204e1a76cf8c15fb4c3ed0206a31a4
NicholasPiano <nicholas.d.piano@gmail.com>
2017-03-18 21:31:07 +0000

    new staging settings

--- scripts/staging_reset.sh
+++ scripts/staging_reset.sh
@@ -2,4 +2,4 @@ sh scripts/remove_migrations.sh
 sh scripts/staging_make_migrations.sh
 rm -rf woot/media/
 python manage.py migrate --settings="woot.settings.staging"
-python manage.py test_data --settings="woot.settings.staging"
+python manage.py test_data_sans_data --settings="woot.settings.staging"
