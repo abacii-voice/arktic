596236d8048182ccbf19788e5e12a6ad6f8bc95b
NicholasPiano <nicholas.d.piano@gmail.com>
2017-04-01 16:41:18 +0100

    Refactor control interface

--- woot/apps/tr/static/tr/js/account.interfaces/account.interfaces.controlInterface.js
+++ woot/apps/tr/static/tr/js/account.interfaces/account.interfaces.controlInterface.js
@@ -15,8 +15,8 @@ AccountInterfaces.controlInterface = function () {
 			},
 		},
 		children: [
-			Components.sidebar('client-sidebar', {
-				ui: {
+			Components.sidebar('clients', {
+				options: {
 					position: {
 						main: {
 							on: '0px',
@@ -34,11 +34,63 @@ AccountInterfaces.controlInterface = function () {
 					},
 				},
 				children: [
-					Components.searchableList('list'),
+					Components.searchableList('list', {
+						options: {
+							title: {
+								text: 'Clients',
+								center: false,
+							},
+							search: {
+								show: false,
+							},
+							mode: 'list', // 3 modes: list, autocomplete, and dropdown
+							targets: [
+								{
+									name: 'clients',
+									path: function () {
+										return Util.ep('clients');
+									},
+									process: function (data) {
+										var results = Object.keys(data).map(function (key) {
+											var client = data[key];
+											return {
+												id: key,
+												main: client.name,
+												rule: 'clients',
+											}
+										});
+										return Util.ep(results);
+									},
+									filterRequest: function () {
+										return {};
+									},
+								},
+							],
+							sort: Util.sort.alpha('main'),
+							unit: {
+								ui: {
+									state: {
+										map: 'role-state',
+									},
+								},
+							},
+						},
+						ui: {
+							state: {
+								states: [
+									UI.state('client-state', {
+										preFn: function (_this) {
+											return _this.control.setup.main();
+										}
+									}),
+								],
+							},
+						},
+					}),
 				],
 			}),
-			Components.sidebar('role-sidebar', {
-				ui: {
+			Components.sidebar('roles', {
+				options: {
 					position: {
 						main: {
 							on: '50px',
@@ -56,11 +108,65 @@ AccountInterfaces.controlInterface = function () {
 					},
 				},
 				children: [
-					Components.searchableList('list'),
+					Components.searchableList('list', {
+						options: {
+							title: {
+								text: 'Roles',
+								center: false,
+							},
+							search: {
+								show: false,
+							},
+							mode: 'list',
+							targets: [
+								{
+									name: 'roles',
+									path: function () {
+										return Active.get('client').then(function (clientId) {
+											return Util.ep(`user.clients.${clientId}.roles`);
+										});
+									},
+									process: function (data) {
+										var results = Object.keys(data).map(function (key) {
+											var role = data[key];
+											return {
+												id: key,
+												main: role.display,
+												rule: 'roles',
+											}
+										});
+										return Util.ep(results);
+									},
+									filterRequest: function () {
+										return {};
+									},
+								},
+							],
+							sort: Util.sort.alpha('main'),
+							unit: {
+								ui: {
+									state: {
+										map: 'control-state',
+									},
+								},
+							},
+						},
+						ui: {
+							state: {
+								states: [
+									UI.state('role-state', {
+										preFn: function (_this) {
+											return _this.control.setup.main();
+										}
+									}),
+								],
+							},
+						},
+					}),
 				],
 			}),
-			Components.sidebar('control-sidebar', {
-				ui: {
+			Components.sidebar('controls', {
+				options: {
 					position: {
 						main: {
 							on: '50px',
@@ -73,47 +179,60 @@ AccountInterfaces.controlInterface = function () {
 					},
 					state: {
 						primary: 'control-state',
-						secondary: ['transcription-state', 'settings-state', 'project-state', '-project-state-project'],
-						deactivate: ['client-state', 'role-state', '-settings-state-shortcuts', '-project-state-focus'],
+						secondary: ['transcription-state', 'settings-state', 'project-state', 'project-state.project'],
+						deactivate: ['client-state', 'role-state', 'settings-state.shortcuts', 'project-state.focus'],
 					},
 				},
 				children: [
 					Components.searchableList('list', {
+						options: {
+							title: {
+								text: 'Menu',
+								center: false,
+							},
+							search: {
+								show: false,
+							},
+							mode: 'list',
+						},
 						children: [
-							Components.button('transcription-button', {
+							Components.button('transcription', {
+								options: {
+									label: 'Transcription',
+								},
 								ui: {
-									appearance: {
-										html: 'Transcription',
-									},
 									state: {
-										stateMap: 'transcription-state',
+										map: 'transcription-state',
+									},
+								}.
+							}),
+							Components.button('settings', {
+								options: {
+									label: 'Settings',
+								},
+								ui: {
+									state: {
+										map: 'settings-state',
 									},
 								},
 							}),
-							Components.button('settings-button', {
+							Components.button('moderation', {
+								options: {
+									label: 'Moderation',
+								},
 								ui: {
-									appearance: {
-										html: 'Settings',
-									},
 									state: {
-										stateMap: 'settings-state',
+										map: 'settings-state',
 									},
 								},
 							}),
-							Components.button('moderation-button', {
-								ui: {
-									appearance: {
-										html: 'Moderation',
-									},
+							Components.button('project', {
+								options: {
+									label: 'Projects',
 								},
-							}),
-							Components.button('project-button', {
 								ui: {
-									appearance: {
-										html: 'Projects',
-									},
 									state: {
-										stateMap: 'project-state',
+										map: 'settings-state',
 									},
 								},
 							}),
@@ -121,7 +240,7 @@ AccountInterfaces.controlInterface = function () {
 					}),
 				],
 			}),
-			Components.sidebar('settings-sidebar', {
+			Components.sidebar('settings', {
 				ui: {
 					position: {
 						main: {
@@ -141,14 +260,24 @@ AccountInterfaces.controlInterface = function () {
 				},
 				children: [
 					Components.searchableList('list', {
+						options: {
+							title: {
+								text: 'Settings',
+								center: false,
+							},
+							search: {
+								show: false,
+							},
+							mode: 'list',
+						},
 						children: [
-							Components.button('shortcuts-button', {
+							Components.button('shortcuts', {
+								options: {
+									label: 'Shortcuts',
+								},
 								ui: {
-									appearance: {
-										html: 'Shortcuts',
-									},
 									state: {
-										stateMap: 'shortcut-state',
+										map: 'shortcut-state',
 									},
 								},
 							}),
@@ -156,60 +285,50 @@ AccountInterfaces.controlInterface = function () {
 					}),
 				],
 			}),
-			Components.actionMasterController('amc'),
+			Components.actionMasterController('amc', {
+				options: {
+					action: {
+						period: 20000,
+					}
+				},
+				ui: {
+					state: {
+						modes: {
+							default: {
+								fn: function (_this) {
+									_this.action.start();
+									return Util.ep();
+								}
+							}
+						},
+						states: [
+							UI.state('client-state', {
+								mode: 'default',
+							}),
+							UI.state('role-state', {
+								mode: 'default',
+							}),
+							UI.state('control-state', {
+								mode: 'default',
+							}),
+							UI.state('transcription-state', {
+								mode: 'default',
+							}),
+						],
+					},
+				},
+			}),
 		],
 	}).then(function (base) {
 
 		// unpack components
 		var amc = base.get('amc');
-		var clientList = base.get('client-sidebar.list');
-		var roleList = base.get('role-sidebar.list');
-		var controlList = base.get('control-sidebar.list');
-		var settingsList = base.get('settings-sidebar.list');
-
-		// action master controller
-		amc.action.period = 20000;
-		amc.state = {
-			defaultState: {
-				fn: function (_this) {
-					_this.action.start();
-					return Util.ep();
-				}
-			},
-			states: {
-				'client-state': 'default',
-				'role-state': 'default',
-				'control-state': 'default',
-				'transcription-state': 'default',
-			},
-		}
+		var clientList = base.get('clients.list');
+		var roleList = base.get('roles.list');
+		var controlList = base.get('controls.list');
+		var settingsList = base.get('settings.list');
 
 		// CLIENT SIDEBAR
-		clientList.autocomplete = false;
-		clientList.targets = [
-			{
-				name: 'clients',
-				path: function () {
-					return Util.ep('clients');
-				},
-				process: function (data) {
-					var results = Object.keys(data).map(function (key) {
-						var client = data[key];
-						return {
-							id: key,
-							main: client.name,
-							rule: 'clients',
-						}
-					});
-					return Util.ep(results);
-				},
-				filterRequest: function () {
-					return {};
-				},
-			},
-		]
-		clientList.sort = Util.sort.alpha('main');
-		clientList.unit.ui.state.stateMap = 'role-state';
 		clientList.unit.bindings = {
 			'click': function (_unit) {
 				amc.addAction({type: 'click.clientlist', metadata: {client: _unit.datum.id}});
@@ -218,144 +337,8 @@ AccountInterfaces.controlInterface = function () {
 				});
 			},
 		}
-		clientList.state = {
-			states: {
-				'client-state': {
-					preFn: function (_this) {
-						return _this.control.setup.main();
-					},
-				},
-			},
-		}
-
-		// MOVE TO SEARCHABLE LIST UNIT
-		function (datum, query, index) {
-			query = (query || '');
-			return UI.createComponent(`${index}`, {
-				ui: {
-					template: UI.template('div', 'ie button'),
-					appearance: {
-						classes: [datum.rule],
-					},
-					state: {
-						stateMap: 'role-state',
-					},
-				},
-
-				children: [
-					// main wrapper
-					UI.createComponent('{base}-main-wrapper'.format({base: baseId}), {
-						name: 'mainWrapper',
-						template: UI.template('div', 'ie centred-vertically'),
-						appearance: {
-							style: {
-								'display': 'inline-block',
-							},
-						},
-						children: [
-							// main
-							UI.createComponent('{base}-mw-head'.format({base: baseId}), {
-								name: 'head',
-								template: UI.template('span', 'ie'),
-								appearance: {
-									style: {
-										'color': Color.grey.normal,
-										'display': 'inline-block',
-										'position': 'absolute',
-									},
-									html: datum.main.substring(0, query.length),
-								},
-							}),
-							UI.createComponent('{base}-mw-tail'.format({base: baseId}), {
-								name: 'tail',
-								template: UI.template('span', 'ie'),
-								appearance: {
-									style: {
-										'display': 'inline-block',
-									},
-									html: datum.main,
-								},
-							}),
-						],
-					}),
-				],
-			}).then(function (unitBase) {
-
-				unitBase.activate = function () {
-					return unitBase.setAppearance({classes: {add: ['active']}});
-				}
-				unitBase.deactivate = function () {
-					return unitBase.setAppearance({classes: {remove: ['active']}});
-				}
-				unitBase.hide = function () {
-					unitBase.isHidden = true;
-					return unitBase.setAppearance({classes: {add: 'hidden'}});
-				}
-				unitBase.show = function () {
-					unitBase.isHidden = false;
-					return unitBase.setAppearance({classes: {remove: 'hidden'}});
-				}
-				unitBase.updateMetadata = function (ndatum, query) {
-					// if there are changes, do stuff.
-					return ((!unitBase.datum || ndatum.id !== unitBase.datum.id) ? unitBase.updateDatum : Util.ep)(ndatum).then(function () {
-						return (query !== unitBase.query ? unitBase.updateQuery : Util.ep)(query);
-					}).then(function () {
-						return (unitBase.isHidden ? unitBase.show : Util.ep)();
-					});
-				}
-				unitBase.updateDatum = function (ndatum) {
-					return unitBase.setAppearance({classes: {add: ndatum.rule, remove: (unitBase.datum || datum).rule}}).then(function () {
-						unitBase.datum = ndatum;
-						return Util.ep();
-					});
-				}
-				unitBase.updateQuery = function (query) {
-					unitBase.query = query;
-					return Promise.all([
-						unitBase.cc.mainWrapper.cc.head.setAppearance({html: (unitBase.datum || datum).main.substring(0, query.length)}),
-						unitBase.cc.mainWrapper.cc.tail.setAppearance({html: (unitBase.datum || datum).main}),
-					]);
-				}
-
-				// complete promises.
-				return Promise.all([
-					unitBase.setBindings({
-
-					}),
-				]).then(function () {
-					return unitBase;
-				});
-			});
-		}
 
 		// ROLE SIDEBAR
-		roleList.autocomplete = false;
-		roleList.targets = [
-			{
-				name: 'roles',
-				path: function () {
-					return Active.get('client').then(function (clientId) {
-						return Util.ep(`user.clients.${clientId}.roles`);
-					});
-				},
-				process: function (data) {
-					var results = Object.keys(data).map(function (key) {
-						var role = data[key];
-						return {
-							id: key,
-							main: role.display,
-							rule: 'roles',
-						}
-					});
-					return Util.ep(results);
-				},
-				filterRequest: function () {
-					return {};
-				},
-			},
-		]
-		roleList.sort = Util.sort.alpha('main');
-		roleList.unit.ui.state.stateMap = 'control-state';
 		roleList.unit.bindings = {
 			'click': function (_unit) {
 				amc.addAction({type: 'click.rolelist', metadata: {role: _unit.data.id}});
@@ -366,85 +349,47 @@ AccountInterfaces.controlInterface = function () {
 		}
 
 		// CONTROL SIDEBAR
-		controlList.get('transcription-button').bindings = {
+		controlList.get('transcription').bindings = {
 			'click': function (_this) {
-				amc.addAction({type: 'click.transcription-button'});
+				amc.addAction({type: 'click.transcription'});
 				return _this.triggerState();
 			},
 		}
-		controlList.get('settings-button').bindings = {
+		controlList.get('settings').bindings = {
 			'click': function (_this) {
-				amc.addAction({type: 'click.settings-button'});
+				amc.addAction({type: 'click.settings'});
 				return _this.triggerState();
 			},
 		}
-		controlList.get('moderation-button').bindings = {
+		controlList.get('moderation').bindings = {
 			'click': function (_this) {
-				amc.addAction({type: 'click.moderation-button'});
+				amc.addAction({type: 'click.moderation'});
 				return _this.triggerState();
 			},
 		}
-		controlList.get('project-button').bindings = {
+		controlList.get('projects').bindings = {
 			'click': function (_this) {
-				amc.addAction({type: 'click.project-button'});
+				amc.addAction({type: 'click.projects'});
 				return _this.triggerState();
 			},
 		}
 
 		// SETTINGS SIDEBAR
-
+		settingsList.get('shortcuts').bindings = {
+			'click': function (_this) {
+				amc.addAction({type: 'click.shortcuts-button'});
+				return _this.triggerState();
+			},
+		}
 
 		// complete promises
 		return Promise.all([
 
 			// CLIENT SIDEBAR
-			clientList.setTitle({text: 'Clients', center: false}),
-			clientList.setSearch({mode: 'off', placeholder: 'Search clients...'}),
 
 			// ROLE SIDEBAR
-			roleList.unitStyle.apply(),
-			roleList.search.setAppearance({
-				style: {
-					'left': '0px',
-					'width': '100%',
-					'height': '30px',
-					'padding-top': '8px',
-				},
-			}),
-			roleList.cc.title.setAppearance({
-				style: {
-					'width': '100%',
-					'height': '32px',
-					'font-size': '18px',
-					'padding-top': '10px',
-					'padding-left': '10px',
-				},
-			}),
-			roleList.setState({
-				states: {
-					'role-state': {
-						preFn: function (_this) {
-							return _this.control.setup.main();
-						},
-					},
-				}
-			}),
-			roleList.setTitle({text: 'Roles', center: false}),
-			roleList.setSearch({mode: 'off', placeholder: 'Search roles...'}),
 
 			// CONTROL SIDEBAR
-			controlList.setTitle({text: 'Menu', center: false}),
-			controlList.setSearch({mode: 'off', placeholder: ''}),
-			controlList.cc.title.setAppearance({
-				style: {
-					'width': '100%',
-					'height': '32px',
-					'font-size': '18px',
-					'padding-top': '10px',
-					'padding-left': '10px',
-				},
-				html: 'Menu',
-			}),
 			controlList.setState({
 				states: {
 					'control-state': {
@@ -483,25 +428,6 @@ AccountInterfaces.controlInterface = function () {
 					},
 				},
 			}),
-
-			// SETTINGS SIDEBAR
-			settingsList.cc.list.cc.wrapper.cc.shortcutsButton.setBindings({
-				'click': function (_this) {
-					amc.addAction({type: 'click.shortcuts-button'});
-					return _this.triggerState();
-				},
-			}),
-			settingsList.cc.title.setAppearance({
-				style: {
-					'width': '100%',
-					'height': '32px',
-					'font-size': '18px',
-					'padding-top': '10px',
-					'padding-left': '10px',
-				},
-			}),
-			settingsList.setTitle({text: 'Settings', center: false}),
-			settingsList.setSearch({mode: 'off', placeholder: ''}),
 
 		]).then(function () {
 			return base;
