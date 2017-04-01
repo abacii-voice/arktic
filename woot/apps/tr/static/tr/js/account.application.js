d211cd5cfa09a59ce86e14e2a4c2610e90d034e2
NicholasPiano <nicholas.d.piano@gmail.com>
2017-04-01 20:56:01 +0100

    Accounts sole function now is to call the App function

--- woot/apps/tr/static/tr/js/account.application.js
+++ woot/apps/tr/static/tr/js/account.application.js
@@ -2,7 +2,6 @@
 
 // ##### version 1
 // 1. Add color and ui measurements to _ variable
-// 2.
 
 var AccountApplication = function () {
 	return UI.createComponent('accountApplication', {
@@ -10,7 +9,7 @@ var AccountApplication = function () {
 			template: 'div.ie',
 			styles: {
 				'.border': {
-					'border': `1px solid ${_.color.grey.normal}`,
+					'border': `${_.ui.dimensions.border} solid ${_.color.grey.normal}`,
 				},
 				'.border-radius': {
 					'border-radius': `${_.ui.dimensions.corner}`,
@@ -24,5 +23,34 @@ var AccountApplication = function () {
 			AccountInterfaces.shortcutInterface(),
 			AccountInterfaces.projectInterface(),
 		],
+	}).then(function (base) {
+		base.initialise = function () {
+			return Promise.all([
+				Context.get('clients').then(function (clients) {
+					var client = Object.keys(clients).filter(function (key) {
+						return clients[key].is_production;
+					})[0];
+					return Promise.all([
+						Active.set('client', client),
+						Context.get(`user.clients.${client}.roles`).then(function (roles) {
+							var role = Object.keys(roles).filter(function (key) {
+								return roles[key].type === 'admin';
+							})[0];
+							return Promise.all([
+								Active.set('role', role),
+								Context.get(`user.clients.${client}.roles.${role}.project`).then(function (project) {
+									return Active.set('project', project);
+								}),
+							]);
+						}),
+					]);
+				}),
+			]).then(function () {
+				// return UI.changeState('clientState');
+				// return UI.changeState('controlState');
+				// return UI.changeState('transcriptionState');
+				return UI.changeState('projectState');
+			});
+		}
 	});
 }
--- woot/apps/tr/static/tr/js/account.js
+++ woot/apps/tr/static/tr/js/account.js
@@ -4,45 +4,8 @@
 // 1. App takes a single argument after the html element,
 // 		which should be an application, but could easily be an explicit component tree. Can be callable or called.
 // 2. Remove render step, which should be implicit in the app function
-// 3.
+// 3. Move initialisation to AccountApplication
 
-UI.app('hook', AccountApplication).then(function (application) {
-
-	// By this point, the rendering has taken place, but no state has been called.
-	return application.initialise();
-	return Promise.all([
-		// client
-		Context.get('clients').then(function (clients) {
-			var clientId = Object.keys(clients).filter(function (key) {
-				return clients[key].is_production;
-			})[0];
-			return Promise.all([
-				Active.set('client', clientId),
-
-				// role
-				Context.get('user.clients.{client}.roles'.format({client: clientId})).then(function (roles) {
-					var roleId = Object.keys(roles).filter(function (key) {
-						return roles[key].type === 'admin';
-					})[0];
-					return Promise.all([
-						// active
-						Active.set('role', roleId),
-
-						// project
-						// Context.get('user.clients.{client}.roles.{role}.project'.format({client: clientId, role: roleId})).then(function (project) {
-						// 	return Active.set('project', project);
-						// }),
-					]);
-				}),
-			]);
-		}),
-	]).then(function () {
-		// return UI.changeState('client-state');
-		// return UI.changeState('control-state');
-		// return UI.changeState('transcription-state');
-		// return UI.changeState('shortcut-state');
-		return UI.changeState('project-state');
-	});
-}).catch(function (error) {
+UI.app('hook', AccountApplication).catch(function (error) {
 	console.log(error);
 });
