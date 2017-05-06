4eb83b90f8c6b675d09635bab1ef2987ff318e3f
NicholasPiano <nicholas.d.piano@gmail.com>
2017-05-06 22:31:38 +0100

    Testing FAQ interface

---
+++ woot/apps/tr/static/tr/js/account.components/account.components.faq.js
@@ -0,0 +1,16 @@
+
+// initialise
+var AccountComponents = (AccountComponents || {});
+
+// faq
+AccountComponents.faq = function (id) {
+	return UI.createComponent(id, {
+		name: 'faq',
+	}).then(function (base) {
+		return Promise.all([
+			
+		]).then(function () {
+			return base;
+		});
+	});
+}
