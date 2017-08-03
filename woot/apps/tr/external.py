a12846f666042191d02f6b5c772271bef2385311
NicholasPiano <nicholas.d.piano@gmail.com>
2017-08-03 19:29:52 +0100

    relative paths

--- woot/apps/tr/external.py
+++ woot/apps/tr/external.py
@@ -1,23 +1,28 @@
 
+# django
+from django.conf import settings
+
+# util
+from os.path import join
 import json
 
 def get_faq():
 	faq = []
-	with open('./faq.json') as open_faq:
+	with open(join(settings.SITE_ROOT, './faq.json')) as open_faq:
 		faq = json.loads(open_faq.read())
 
 	return faq
 
 def get_rules():
 	rules = []
-	with open('./rules.json') as open_rules:
+	with open(join(settings.SITE_ROOT, './rules.json')) as open_rules:
 		rules = json.loads(open_rules.read())
 
 	return rules
 
 def get_shortcuts():
 	shortcuts = []
-	with open('./shortcuts.json') as open_shortcuts:
+	with open(join(settings.SITE_ROOT, './shortcuts.json')) as open_shortcuts:
 		shortcuts = json.loads(open_shortcuts.read())
 
 	return shortcuts
